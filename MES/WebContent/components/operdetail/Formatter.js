073d27eb19216fc2b3b896d61e92c2f1c09a5263
vinay singh <vinay.singh@soprasteria.com>
2016-10-21 12:22:16 +0530

    commit

---
+++ MES/WebContent/components/operdetail/Formatter.js
@@ -0,0 +1,90 @@
+"use strict";
+
+
+jQuery.sap.declare("airbus.mes.operationdetail.Formatter");
+
+airbus.mes.operationdetail.Formatter = {
+		setSliderStatus: function(status, progress){
+			switch (status) {
+				case airbus.mes.worktracker.util.Formatter.status.completed:
+					return "Confirmed";
+		
+				case airbus.mes.worktracker.util.Formatter.status.active:
+					return "In Progress";
+					
+				case airbus.mes.worktracker.util.Formatter.status.paused:
+					if(progress == "0.0" || progress == "0" || progress == 0)
+						return "Not Started";
+					else
+						return "Paused "+String(progress).split(".")[0]+"%";
+				default:
+					return "Not Started";
+			}
+		},
+		sliderStatusFirst:function(status, progress){
+		if(typeof progress == "undefined") return;
+		
+		this.removeStyleClass("dynProgressSlider");
+		this.setVisible(true);
+		this.removeStyleClass("sliderBlockedColor");
+		this.addStyleClass("sliderCompletedColor");
+		
+		//to manage blocked status for next version
+		if(status == "B"){
+			this.removeStyleClass("sliderCompletedColor");
+			this.addStyleClass("sliderBlockedColor");
+		}
+		
+		
+		if(progress == "0.0" || progress == "0" || progress == 0){
+			//this.addStyleClass("displaySliderNone");
+			this.setVisible(false);
+			this.setProperty("max",0);
+			this.setProperty("value",0);
+			return "0%";
+		}
+		else if (String(progress) == "100") {
+			this.setProperty("max",100);
+			this.setProperty("value",100);
+			return progress+"%";
+		    
+		}
+		else{
+			this.setProperty("max",parseInt(progress));
+			this.setProperty("value",parseInt(progress));
+			return progress+"%";
+			
+		}
+	},
+	
+
+	sliderStatus : function(status, progress) {
+		
+		if(typeof progress == "undefined") return;
+		
+		this.removeStyleClass("dynProgressSlider");
+		this.setVisible(true);
+		
+
+		if (progress == "0.0" || progress == "0" || progress == 0 || progress==NaN) {
+			this.removeStyleClass("dynProgressSlider");
+			this.setProperty("min", 0);
+			this.setProperty("value", 0);
+			return "100%";
+		}
+		else if (String(progress) == "100") {
+			this.setVisible(false);
+			return "0%";
+		}
+		else{
+			this.addStyleClass("dynProgressSlider");
+			this.setProperty("min", parseInt(progress));
+			this.setProperty("value", parseInt(progress));
+			return (100 - parseInt(progress)) + "%";
+			
+		}
+	
+		
+	}
+	
+};
