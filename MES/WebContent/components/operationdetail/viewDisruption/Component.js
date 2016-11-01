148658fd294ae0d25c0795bbec3d71784ceddf3f
Masoom Jajoo <masoom.jajoo@soprasteria.com>
2016-11-01 18:52:35 +0530

    OperationDetailPopup

--- MES/WebContent/components/operationdetail/viewDisruption/Component.js
+++ MES/WebContent/components/operationdetail/viewDisruption/Component.js
@@ -3,28 +3,28 @@ jQuery.sap.registerModulePath("airbus.mes.disruptions", "../components/disruptio
 //jQuery.sap.require("airbus.mes.disruptions.util.Formatter");
 //jQuery.sap.require("airbus.mes.disruptions.ModelManager");
 
-jQuery.sap.declare("airbus.mes.operationdetail.createDisruption.Component");
+jQuery.sap.declare("airbus.mes.operationdetail.viewDisruption.Component");
 
-sap.ui.core.UIComponent.extend("airbus.mes.operationdetail.createDisruption.Component", {
+sap.ui.core.UIComponent.extend("airbus.mes.operationdetail.viewDisruption.Component", {
 	metadata : {
 		properties : {},
-		includes : [ "../../disruptions/css/createDisruption.css" ]
+		includes : [ "../../disruptions/css/viewDisruption.css" ]
 	// array of css and/or javascript files that should be used in the component
 
 	}
 
 });
 
-airbus.mes.operationdetail.createDisruption.Component.prototype.createContent = function() {
+airbus.mes.operationdetail.viewDisruption.Component.prototype.createContent = function() {
 	
-	if (airbus.mes.operationdetail.createDisruption.oView === undefined) {
+	if (airbus.mes.operationdetail.viewDisruption.oView === undefined) {
 //		Initialization
 		//airbus.mes.disruptions.ModelManager.init(sap.ui.getCore());
 		
 		// View on XML
 		this.oView = sap.ui.view({
-			id : "createDisruptionView",
-			viewName : "airbus.mes.disruptions.CreateDisruption",
+			id : "ViewDisruptionView",
+			viewName : "airbus.mes.disruptions.ViewDisruption",
 			type : "XML",
 			height:"100%"
 		})
@@ -33,7 +33,7 @@ airbus.mes.operationdetail.createDisruption.Component.prototype.createContent = 
 	        bundleUrl : "../components/disruptions/i18n/i18n.properties"
 	    });
 		this.oView.setModel(i18n, "i18n");	
-		airbus.mes.operationdetail.createDisruption.oView = this.oView		
+		airbus.mes.operationdetail.viewDisruption.oView = this.oView		
 		
 		
 		/*//Model for disruptions list data in table
