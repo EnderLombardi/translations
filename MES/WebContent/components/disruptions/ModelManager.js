fc40192fe618292b62e3b144e16a7587a6616bba
Anant Kamal <anant.kamal@soprasteria.com>
2016-11-02 16:14:58 +0530

    commit

---
+++ MES/WebContent/components/disruptions/ModelManager.js
@@ -0,0 +1,43 @@
+//"use strict";
+jQuery.sap.declare("airbus.mes.disruptions.ModelManager")
+airbus.mes.disruptions.ModelManager = {
+	urlModel : undefined,
+	queryParams : jQuery.sap.getUriParameters(),
+
+	init : function(core) {
+		
+		core.setModel(new sap.ui.model.json.JSONModel(), "DisruptionDetail");//Model having disruptions detail
+		core.setModel(new sap.ui.model.resource.ResourceModel({bundleName:"airbus.mes.disruptions.i18n.i18n",bundleLocale:"en"}), 
+														 "disruptionsI18n");
+
+
+		var dest;
+
+		switch (window.location.hostname) {
+		case "localhost":
+			dest = "local";
+			break;
+		default:
+			dest = "local";
+			break;
+		}
+		
+		if (this.queryParams.get("url_config")) {
+			dest = this.queryParams.get("url_config");
+		}
+
+		this.urlModel = new sap.ui.model.resource.ResourceModel({
+			bundleUrl : "../components/disruptions/config/url_config.properties",
+			bundleLocale : dest
+		});
+		
+		this.loadData();
+		
+	},
+	
+	loadData: function() {
+		var oViewModel = sap.ui.getCore().getModel("DisruptionDetail");
+		oViewModel.loadData(this.urlModel.getProperty("urldisruptionmodel"), null, false);
+	}
+	
+}
