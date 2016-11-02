0bc21a68851db8b51360886dbeb43caf191d2862
Masoom Jajoo <masoom.jajoo@soprasteria.com>
2016-11-02 17:45:41 +0530

    createDisrupion

--- MES/WebContent/components/operationdetail/createDisruption/Component.js
+++ MES/WebContent/components/operationdetail/createDisruption/Component.js
@@ -1,7 +1,7 @@
 jQuery.sap.require("sap.ui.core.UIComponent");
 jQuery.sap.registerModulePath("airbus.mes.disruptions", "../components/disruptions");
 //jQuery.sap.require("airbus.mes.disruptions.util.Formatter");
-//jQuery.sap.require("airbus.mes.disruptions.ModelManager");
+jQuery.sap.require("airbus.mes.disruptions.ModelManager");
 
 jQuery.sap.declare("airbus.mes.operationdetail.createDisruption.Component");
 
@@ -19,7 +19,7 @@ airbus.mes.operationdetail.createDisruption.Component.prototype.createContent = 
 	
 	if (airbus.mes.operationdetail.createDisruption.oView === undefined) {
 //		Initialization
-		//airbus.mes.disruptions.ModelManager.init(sap.ui.getCore());
+		airbus.mes.disruptions.ModelManager.init(sap.ui.getCore());
 		
 		// View on XML
 		this.oView = sap.ui.view({
@@ -36,15 +36,8 @@ airbus.mes.operationdetail.createDisruption.Component.prototype.createContent = 
 		airbus.mes.operationdetail.createDisruption.oView = this.oView		
 		
 		
-		/*//Model for disruptions list data in table
-		this.oView.setModel(sap.ui.getCore().getModel("disruptionsTrackerModel"),"disruptionsTrackerModel");
-		
-		//Model for disruptions filter data in ComboBox
-		this.oView.setModel(sap.ui.getCore().getModel("disruptionsFilterData"), "disruptionsFilterData");
-		
-		//Model for disruptions order data in ComboBox
-		this.oView.setModel(sap.ui.getCore().getModel("disruptionsOrderData"), "disruptionsOrderData");*/
-		
+		//Model for custom data of create disruption
+		this.oView.setModel(sap.ui.getCore().getModel("disruptionCustomData"),"disruptionCustomData");
 		
 		return this.oView;
 		
