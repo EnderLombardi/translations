ada9d4c29a360d55a0c3c1fe68258cf53604a8a6
Chirag Pahuja <chirag.pahuja@soprasteria.com>
2016-11-03 20:11:34 +0530

    Edit Disruption view initialized

---
+++ MES/WebContent/components/disruptions/EditDisruption.controller.js
@@ -0,0 +1,279 @@
+sap.ui
+		.controller(
+				"airbus.mes.disruptions.EditDisruption",
+				{
+
+					/**
+					 * Called when a controller is instantiated and its View
+					 * controls (if available) are already created. Can be used
+					 * to modify the View before it is displayed, to bind event
+					 * handlers and do other one-time initialization.
+					 * 
+					 * @memberOf airbus.mes.components.disruptions.EditDisruption
+					 */
+	/*				onInit : function() {
+						// this.loadDisruptionCustomData();
+						this.addParent(this.selectTree, undefined);
+					},*/
+
+					/**
+					 * Similar to onAfterRendering, but this hook is invoked
+					 * before the controller's View is re-rendered (NOT before
+					 * the first rendering! onInit() is used for that one!).
+					 * 
+					 * @memberOf airbus.mes.components.disruptions.EditDisruption
+					 */
+					// onBeforeRendering: function() {
+					//
+					// },
+					selectTree : {
+						id : "selectCategory",
+						type : "select",
+						path : "MessageType",
+						attr : "MessageType",
+						childs : [ {
+							id : "selectReasonTree",
+							type : "select",
+							path : "Reason",
+							attr : "Reason",
+							childs : [ {
+								id : "selectResponsible",
+								type : "select",
+								path : "ResponsibleGroup",
+								attr : "ResponsibleGroup",
+								childs : [ {
+									id : "selectRootCause",
+									type : "select",
+									path : "rootCause",
+									attr : "rootCause",
+									childs: []}, {
+									id : "Return",
+									type : "Return",
+									childs : []
+								} ]
+							} ]
+						}, ]
+					},
+					ModelManager : undefined,
+					onInit : function() {
+
+						this.addParent(this.selectTree, undefined);
+						this.ModelManager = airbus.mes.disruptions.ModelManager;
+
+						// airbus.mes.settings.ModelManager.loadUserSettingsModel();
+					},
+
+					addParent : function(oTree, oParent) {
+						var that = this;
+						oTree.parent = oParent;
+						oTree.childs.forEach(function(oElement) {
+							that.addParent(oElement, oTree);
+						});
+					},
+					findElement : function(oTree, sId) {
+						if (oTree.id == sId) {
+							return oTree;
+						} else {
+							var oElement;
+							for (var i = 0; i < oTree.childs.length; i++) {
+								oElement = this.findElement(oTree.childs[i],
+										sId);
+								if (oElement) {
+									return oElement;
+								}
+							}
+						}
+					},
+
+					// *******************on change of item in the ComboBox
+					// *******************
+					onSelectionChange : function(oEvt) {
+						var that = this;
+						var id = oEvt.getSource().getId().split("--")[1];
+						this.findElement(this.selectTree, oEvt.getSource()
+								.getId().split("--")[1]).childs
+								.forEach(function(oElement) {
+									that.clearField(oElement);
+									that.filterField(oElement);
+								});
+
+					/*	if (id === "selectCatogory") {
+							this.setEnabledSelectBox(true, true, false, false);
+						} else if (id === "selectReasonTree") {
+							this.setEnabledSelectBox(true, true, true, false);
+						} else {
+							this.setEnabledSelectBox(true, true, true, true);
+						}*/
+
+					},
+
+					// ****************** clear other ComboBoxes after changing
+					// the item of one comboBox *****
+					clearField : function(oTree) {
+						var that = this;
+						if (oTree.type == "select") {
+							that.getView().byId(oTree.id).setSelectedKey();
+						}
+						oTree.childs.forEach(that.clearField.bind(that));
+					},
+
+					filterField : function(oTree) {
+						if (oTree.type == "Return") {
+							return;
+						}
+						var that = this;
+						var aFilters = [];
+						var oElement = oTree.parent;
+						while (oElement) {
+							var val = this.getView().byId(oElement.id)
+									.getSelectedKey();
+							if (val) {
+								var oFilter = new sap.ui.model.Filter(
+										oElement.path, "EQ", val);
+								aFilters.push(oFilter);
+							}
+							;
+							oElement = oElement.parent;
+						}
+						;
+						var temp = [];
+						var duplicatesFilter = new sap.ui.model.Filter({
+							path : oTree.path,
+							test : function(value) {
+								if (temp.indexOf(value) == -1) {
+									temp.push(value)
+									return true;
+								} else {
+									return false;
+								}
+							}
+						});
+						aFilters.push(duplicatesFilter);
+						if (this.getView().byId(oTree.id).getBinding("items"))
+							this.getView().byId(oTree.id).getBinding("items")
+									.filter(
+											new sap.ui.model.Filter(aFilters,
+													true));
+
+						oTree.childs.forEach(function(oElement) {
+							that.filterField(oElement);
+						});
+					},
+					loadDisruptionCustomData : function(oEvt) {
+						/*
+						 * airbus.mes.settings.ModelManager.site =
+						 * this.getView() .byId("ComboBoxPlant").getValue();
+						 * airbus.mes.settings.ModelManager.loadPlantModel();
+						 */
+						airbus.mes.disruption.ModelManager
+								.loadDisruptionCustomData();
+						this.filterField(this.selectTree);
+						this.getView().byId("selectCategory").setValue("");
+						this.getView().byId("selectReasonTree").setValue("");
+						this.getView().byId("selectRootCause").setValue("");
+						this.getView().byId("selectResponsible").setValue("");
+
+						this.setEnabledSelectBox(true, false, false, false);
+					},
+
+					setEnabledSelectBox : function(fCategory, fReason,
+							fResponsible, fRootCause) {
+						this.getView().byId("selectCategory").setEnabled(
+								fCategory);
+						this.getView().byId("selectReasonTree").setEnabled(
+								fReason);
+						this.getView().byId("selectRootCause").setEnabled(
+								fResponsible);
+						this.getView().byId("selectResponsible").setEnabled(
+								fRootCause);
+
+					},
+					/***********************************************************
+					 * Edit Disruption
+					 */
+					onEditDisrupution : function() {
+
+						var sCategory = this.getView().byId("selectCategory").getSelectedKey();
+						var sRootCause = this.getView().byId("selectRootCause").getSelectedKey();			
+						var sComment = this.getView().byId("comment").getValue();
+
+						
+						// Create a JSON for payload attributes
+						var aModelData = []
+						
+
+							var oJson = {
+								"payload":[{
+									"attribute" : "OPERATION_BO",
+									"value": sap.ui.getCore().getModel("operationDetailModel").getProperty("/Rowsets/Rowset/0/Row/0/operation_bo"),
+								},
+								{
+									"attribute" : "SFC_BO",
+									"value": sap.ui.getCore().getModel("operationDetailModel").getProperty("/Rowsets/Rowset/0/Row/0/sfc"),	
+								},
+								{
+									"attribute" : "SFC_STEP_BO",
+									"value": sap.ui.getCore().getModel("operationDetailModel").getProperty("/Rowsets/Rowset/0/Row/0/sfc_step_ref"),	
+								},
+								{
+									"attribute" : "DESCRIPTION",
+									"value": this.getView().byId("comment").getValue()
+								},
+								{
+									"attribute" : "REASON",
+									"value": this.getView().byId("selectReasonTree").getSelectedKey()
+								},
+								{
+									"attribute" : "TIME_LOST",
+									"value": this.getView().byId("timeLost").getValue()
+								},
+								{
+									"attribute" : "REQD_FIX_BY",
+									"value": this.getView().byId("expectedTime").getValue()	
+								},
+								{
+									"attribute" : "GRAVITY",
+									"value": this.getView().byId("gravity").getSelectedKey()
+								},
+								{
+									"attribute" : "STATUS",
+									"value": this.getView().getModel("i18nModel").getProperty("Pending")
+								},
+								{
+									"attribute" : "ROOT_CAUSE",
+									"value": this.getView().byId("selectRootCause").getSelectedKey()
+								},
+								{
+									"attribute" : "MSN",
+									"value": airbus.mes.settings.ModelManager.msn
+								}
+								]
+								
+							}
+							aModelData.push(oJson);
+
+							
+						airbus.mes.disruptions.ModelManager.createDisruption(sCategory,sRootCause,sComment,aModelData);
+					},
+
+				/**
+				 * Called when the View has been rendered (so its HTML is part
+				 * of the document). Post-rendering manipulations of the HTML
+				 * could be done here. This hook is the same one that SAPUI5
+				 * controls get after being rendered.
+				 * 
+				 * @memberOf airbus.mes.components.disruptions.EditDisruption
+				 */
+				 onAfterRendering: function() {
+					// this.filterField(this.selectTree);
+				 },
+				/**
+				 * Called when the Controller is destroyed. Use this one to free
+				 * resources and finalize activities.
+				 * 
+				 * @memberOf airbus.mes.components.disruptions.EditDisruption
+				 */
+				// onExit: function() {
+				//
+				// }
+				});
---
+++ MES/WebContent/components/operationdetail/editDisruption/Component.js
@@ -0,0 +1,45 @@
+jQuery.sap.require("sap.ui.core.UIComponent");
+jQuery.sap.registerModulePath("airbus.mes.disruptions", "../components/disruptions");
+jQuery.sap.require("airbus.mes.disruptions.Formatter");
+jQuery.sap.require("airbus.mes.disruptions.ModelManager");
+
+jQuery.sap.declare("airbus.mes.operationdetail.editDisruption.Component");
+
+sap.ui.core.UIComponent.extend("airbus.mes.operationdetail.findDisruption.Component", {
+	metadata : {
+		properties : {},
+		includes : [ "../../disruptions/css/createDisruption.css" ]
+	// array of css and/or javascript files that should be used in the component
+
+	}
+
+});
+
+airbus.mes.operationdetail.editDisruption.Component.prototype.createContent = function() {
+	
+	if (airbus.mes.operationdetail.editDisruption.oView === undefined) {
+//		Initialization
+		airbus.mes.disruptions.ModelManager.init(sap.ui.getCore());
+		
+		// View on XML
+		this.oView = sap.ui.view({
+			id : "editDisruptionView",
+			viewName : "airbus.mes.disruptions.EditDisruption",
+			type : "XML",
+			height:"auto"
+		})
+
+		var i18nModel = new sap.ui.model.resource.ResourceModel({
+	        bundleUrl : "../components/disruptions/i18n/i18n.properties"
+	    });
+		this.oView.setModel(i18nModel, "i18nModel");	
+		airbus.mes.operationdetail.editDisruption.oView = this.oView		
+		
+		
+		//Model for custom data of edit disruption
+		this.oView.setModel(sap.ui.getCore().getModel("disruptionCustomData"),"disruptionCustomData");
+		
+		return this.oView;
+		
+	}
+};
