5c9f4ce34afa8cad6bc5ebb76a1fb0eccb4ab7a7
njanvier <njanvier@ITEM-S30217.dhcp.colo.fr.sopra>
2016-11-08 16:58:53 +0100

    [Shell] Handle MII messages done.

--- MES/WebContent/shell/util/Formatter.js
+++ MES/WebContent/shell/util/Formatter.js
@@ -51,4 +51,32 @@ airbus.mes.shell.util.Formatter = {
 					+ " " + sLine;
 		}
 	},
+	
+	getMiiText : function(sID, sParam1, sParam2, sParam3, sParam3, sParam4, sParam5, sParam6, sParam6, sParam7, sParam8, sParam9, sParam10) {
+		var sMessage = airbus.mes.shell.oView.getModel("miiI18n").getProperty(sID);
+		if(sParam1.constructor === String){
+			var aParams = Array.from(arguments).slice(1);
+		}else if(sParam1.constructor === Array){
+			var aParams = sParam1;
+		}
+		return jQuery.sap.formatMessage(sMessage, aParams)
+	},
+	
+	getMiiTextFromData : function(data) {
+		if(data.hasOwnProperty("Rowsets")){
+			data = data.Rowsets.Rowset[0].Row[0];
+		}
+		var sMessageID = data.Message_ID;
+		var aParams = [];
+		if(data.hasOwnProperty("Message_ID")){
+			for(var property in data){
+				if(property != "Message_ID" && property !="Message_Type" && data[property] != ""){
+					aParams.push(data[property]);
+				}
+			}
+			this.getMiiText(sMessageID, aParams);
+		}else{
+			console.log("Parameter data cannot be processed");
+		}
+	},
 };
