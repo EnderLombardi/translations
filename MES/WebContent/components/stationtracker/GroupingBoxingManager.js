8d0d11fc238ff609ca537664bf773e0b11711af1
Valentin Vincent <valentin.vincent2@soprasteria.com>
2016-11-10 14:00:00 +0100

    [station tracker] SD-PPC-ST-2600 ATA grouping fix

--- MES/WebContent/components/stationtracker/GroupingBoxingManager.js
+++ MES/WebContent/components/stationtracker/GroupingBoxingManager.js
@@ -299,7 +299,7 @@ airbus.mes.stationtracker.GroupingBoxingManager	 = {
 					"ANDONS": el.ANDONS,
 					"RMA_STATUS_COLOR": fRMA,
 					"status" : sStatus,
-					//"ata": el.ata,
+					"ATA": el.ata,
 					//"familyTarget": el.familyTarget,
 					"CPP_CLUSTER" : el.CPP_CLUSTER,
 					"WORK_PACKAGE" : el.WORK_PACKAGE,
