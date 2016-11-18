af069f9e7e6a8983b3c0d682c3079bcd7a538fe5
Piyush Aggarwal <piyush.aggarwal@soprasteria.com>
2016-11-18 14:48:41 +0530

    [Operation Status] - Station Tracker no more re-rerendered after activation or confirmation

--- MES/WebContent/components/operationdetail/status/status.controller.js
+++ MES/WebContent/components/operationdetail/status/status.controller.js
@@ -62,13 +62,7 @@ sap.ui
 								"progressSlider")
 						oProgressSlider.stepDown(1);
 					},
-					/*
-					 * onCloseOperationDetailPopup : function() {
-					 * 
-					 * airbus.mes.stationtracker.operationDetailPopup.close();
-					 * airbus.mes.shell.oView.getController().getController()
-					 * .renderStationTracker(); },
-					 */
+
 
 					/***********************************************************
 					 * 
@@ -116,10 +110,8 @@ sap.ui
 									}
 								});
 
-						// Refresh User Operation Model and Operation Detail
-						airbus.mes.shell.oView.getController()
-								.renderStationTracker();
 
+						// Refresh User Operation Model and Operation Detail
 						if (flag_success == true) {
 							oView.getController().setProgressScreenBtn(true,
 									false);
@@ -129,8 +121,8 @@ sap.ui
 											"in_progress"));
 
 							// Re-Render Station Tracker
-							airbus.mes.shell.oView.getController()
-									.renderStationTracker();
+							/*airbus.mes.shell.oView.getController()
+									.renderStationTracker();*/
 
 							// update operationDetailsModel
 
@@ -140,6 +132,11 @@ sap.ui
 											"IN_WORK")
 							sap.ui.getCore().getModel("operationDetailModel")
 									.refresh();
+							
+							
+							// Refresh Station tracker Gantt Chart
+							/*airbus.mes.shell.oView.getController()
+									.renderStationTracker();*/
 
 						}
 					},
@@ -192,8 +189,8 @@ sap.ui
 											.getProperty("paused"));
 
 							// Re-Render Station Tracker
-							airbus.mes.shell.oView.getController()
-									.renderStationTracker();
+							/*airbus.mes.shell.oView.getController()
+									.renderStationTracker();*/
 
 							// update operationDetailsModel
 
@@ -533,8 +530,8 @@ sap.ui
 							if (flag_success === true) {
 								// Refresh User Operation Model and Operation
 								// Detail
-								airbus.mes.shell.oView.getController()
-										.renderStationTracker();
+								/*airbus.mes.shell.oView.getController()
+										.renderStationTracker();*/
 
 								// update operationDetailsModel
 								sap.ui.getCore().getModel(
