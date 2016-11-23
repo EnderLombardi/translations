d79c1964a5e8709e2f38740abd67e3aaba186218
Chirag Pahuja <chirag.pahuja@soprasteria.com>
2016-11-23 13:18:37 +0530

    [disruptiontracker] responsive chart

--- MES/WebContent/components/disruptiontracker/kpi/disruptionKPIChart.controller.js
+++ MES/WebContent/components/disruptiontracker/kpi/disruptionKPIChart.controller.js
@@ -13,40 +13,93 @@ sap.ui.controller("airbus.mes.disruptiontracker.kpi.disruptionKPIChart", {
 //     Title For Category
 		var vizframe1 = this.getView().byId("vizFrame");
 		vizframe1.setVizProperties({
-            title : { 
-					visible : true, 
-					text : 'Lost Time (minutes) per Category'
-					} 
-	            });
+			title : { 
+				visible : true, 
+				text : 'Lost Time (minutes) per Category'
+			},
+			
+			general: {
+				layout: {
+					padding : 2
+				}
+			},
+			
+			categoryAxis: {
+				title: {
+					visible: true
+				},
+				
+				label : {
+					visible : true,
+					truncatedLabelRatio: 1,
+					angle : 0,
+					linesOfWrap : 4,
+					style: {
+						fontSize: "10px"
+					}
+				}
+			},
+		});
 	
 		
 		
 //		Title For Reason
 		var vizframe2 = this.getView().byId("vizFrame2");
 		vizframe2.setVizProperties({
-            title : { 
-					visible : true, 
-					text : 'Lost Time (minutes) per Reason'
-					} 
-	            });
+			title : { 
+				visible : true, 
+				text : 'Lost Time (minutes) per Reason'
+			},
+			
+			general: {
+				layout: {
+					padding : 2
+				}
+			},
+
+			categoryAxis: {
+				title: {
+					visible: true
+				},
+				label : {
+					visible : true,
+					truncatedLabelRatio: 1,
+					angle : 0,
+					linesOfWrap : 4,
+					style: {
+						fontSize: "10px"
+					}
+				}
+			},
+		});
 				
 //		Title For Operation 
 		var vizframe3 = this.getView().byId("vizFrame3");
 		vizframe3.setVizProperties({
-            title : { 
-					visible : true, 
-					text : 'Lost Time (minutes) per Operation'
-					} 
-	            });
+			title : { 
+				visible : true, 
+				text : 'Lost Time (minutes) per Operation'
+			},
+			general: {
+				layout: {
+					padding : 2
+				}
+			},
+		});
 		
 //		Title For MSN
 		var vizframe4 = this.getView().byId("vizFrame4");
 		vizframe4.setVizProperties({
-            title : { 
-					visible : true, 
-					text : 'Lost Time (minutes) per MSN'
-					} 
-	            });
+			title : { 
+				visible : true, 
+				text : 'Lost Time (minutes) per MSN'
+			},
+			general: {
+				layout: {
+					padding : 2
+				}
+			},
+		});
 		
 		
 	},
