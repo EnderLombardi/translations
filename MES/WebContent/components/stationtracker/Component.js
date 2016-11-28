9ec5d36cfc4dac775aeec5a3e8133d7842f66694
André Gomes <andre.gomez@ext.soprasteria.com>
2016-11-28 12:16:59 +0100

    [stationtracker] fix error to display station tracker

--- MES/WebContent/components/stationtracker/Component.js
+++ MES/WebContent/components/stationtracker/Component.js
@@ -40,27 +40,43 @@ airbus.mes.stationtracker.Component.prototype.createContent = function() {
     airbus.mes.stationtracker.isDisplay = true;
 
     if (airbus.mes.stationtracker.oView === undefined) {
-//        Initialization
+        //        Initialization
         airbus.mes.stationtracker.ModelManager.init(sap.ui.getCore());
 
-		this.oView.setModel(new sap.ui.model.json.JSONModel(),"productionGroupDisplay");
-		this.oView.setModel(sap.ui.getCore().getModel("stationTrackerShift"),"stationTrackerShift");
-		this.oView.setModel(sap.ui.getCore().getModel("productionGroupModel"), "productionGroupModel");
-		this.oView.setModel(sap.ui.getCore().getModel("KPI"), "KPI");
-		this.oView.setModel(sap.ui.getCore().getModel("KPIextraWork"), "KPIextraWork");
-		this.oView.setModel(sap.ui.getCore().getModel("KPItaktAdherence"), "KPItaktAdherence");
-		this.oView.setModel(sap.ui.getCore().getModel("KPIshiftStaffing"), "KPIshiftStaffing");
-		this.oView.setModel(sap.ui.getCore().getModel("KPItaktEfficiency"), "KPItaktEfficiency");
-		this.oView.setModel(sap.ui.getCore().getModel("KPIresolutionEfficiency"), "KPIresolutionEfficiency");
-		this.oView.setModel(sap.ui.getCore().getModel("KPIdisruption"), "KPIdisruption");
-		this.oView.setModel(sap.ui.getCore().getModel("groupModel"), "groupModel");
-		this.oView.setModel(sap.ui.getCore().getModel("affectationModel"), "affectationModel");
-		this.oView.setModel(sap.ui.getCore().getModel("ressourcePoolModel"), "ressourcePoolModel");
-		this.oView.setModel(sap.ui.getCore().getModel("disruptionAndonKPI"), "disruptionAndonKPI");
-		
-	/*	airbus.mes.disruptiontracker.oView = this.odisruptiontrackerComp.oView;*/
-		return this.oView;
-	} else {
-		return airbus.mes.stationtracker.oView;
-	}
+        // View on XML
+        this.oView = sap.ui.view({
+            id : "stationTrackerView",
+            viewName : "airbus.mes.stationtracker.stationtracker",
+            type : "XML",
+        });
+
+        var i18nModel = new sap.ui.model.resource.ResourceModel({
+            bundleUrl : "../components/stationtracker/i18n/i18n.properties",
+            //            bundleLocale : "en" automatic defined by parameter sap-language
+        });
+        this.oView.setModel(i18nModel, "StationTrackerI18n");
+        // Set instant display for busy indicator
+        this.oView.byId("stationtracker").setBusyIndicatorDelay(0);
+        airbus.mes.stationtracker.oView = this.oView;
+
+        this.oView.setModel(new sap.ui.model.json.JSONModel(),"productionGroupDisplay");
+        this.oView.setModel(sap.ui.getCore().getModel("stationTrackerShift"),"stationTrackerShift");
+        this.oView.setModel(sap.ui.getCore().getModel("productionGroupModel"), "productionGroupModel");
+        this.oView.setModel(sap.ui.getCore().getModel("KPI"), "KPI");
+        this.oView.setModel(sap.ui.getCore().getModel("KPIextraWork"), "KPIextraWork");
+        this.oView.setModel(sap.ui.getCore().getModel("KPItaktAdherence"), "KPItaktAdherence");
+        this.oView.setModel(sap.ui.getCore().getModel("KPIshiftStaffing"), "KPIshiftStaffing");
+        this.oView.setModel(sap.ui.getCore().getModel("KPItaktEfficiency"), "KPItaktEfficiency");
+        this.oView.setModel(sap.ui.getCore().getModel("KPIresolutionEfficiency"), "KPIresolutionEfficiency");
+        this.oView.setModel(sap.ui.getCore().getModel("KPIdisruption"), "KPIdisruption");
+        this.oView.setModel(sap.ui.getCore().getModel("groupModel"), "groupModel");
+        this.oView.setModel(sap.ui.getCore().getModel("affectationModel"), "affectationModel");
+        this.oView.setModel(sap.ui.getCore().getModel("ressourcePoolModel"), "ressourcePoolModel");
+        this.oView.setModel(sap.ui.getCore().getModel("disruptionAndonKPI"), "disruptionAndonKPI");
+
+        /*    airbus.mes.disruptiontracker.oView = this.odisruptiontrackerComp.oView;*/
+        return this.oView;
+    } else {
+        return airbus.mes.stationtracker.oView;
+    }
 };
