a69ec19945a4840586928e0ccdb46856ff66b0eb
Valentin Vincent <valentin.vincent2@soprasteria.com>
2016-12-05 14:06:44 +0100

    [StationTracker Unplanned] select all button

--- MES/WebContent/components/stationtracker/stationtracker.controller.js
+++ MES/WebContent/components/stationtracker/stationtracker.controller.js
@@ -541,6 +541,16 @@ sap.ui.controller("airbus.mes.stationtracker.stationtracker", {
 			
 	},
 	
+	/**
+	 * 
+	 */
+	onUnplannedSelectAll : function(oEvent) {
+		
+		var oList = airbus.mes.stationtracker.ImportOswUnplannedPopover.getContent()[0].getItems()[1];
+		oList.selectAll();
+
+	},
+	
 	onUnplannedClose : function(oEvent) {
 		
 		airbus.mes.stationtracker.worklistPopover.close();
