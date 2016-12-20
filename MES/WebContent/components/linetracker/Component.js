db2eaa13e135746f7c5ed15b97b5fcd68c7623e2
André Gomes <andre.gomez@ext.soprasteria.com>
2016-12-20 13:36:37 +0100

    [linetracker] trad

--- MES/WebContent/components/linetracker/Component.js
+++ MES/WebContent/components/linetracker/Component.js
@@ -10,54 +10,54 @@ jQuery.sap.declare("airbus.mes.linetracker.Component");
 
 // Extend current Component
 sap.ui.core.UIComponent.extend("airbus.mes.linetracker.Component", {
-	//manifestUrl : "Component.json",
-	metadata : {
+    //manifestUrl : "Component.json",
+    metadata : {
 
-		properties : {
-//			textButtonTo : "string",
-//			buttonAction : "string"
-		}
-	}
+        properties : {
+//            textButtonTo : "string",
+//            buttonAction : "string"
+        }
+    }
 
 });
 
 // override the createContent function to return user interface
 airbus.mes.linetracker.Component.prototype.createContent = function() {
 
-	if (airbus.mes.linetracker.oView === undefined) {
-		airbus.mes.linetracker.util.RoleManager.init(this);
-		airbus.mes.linetracker.util.ModelManager.init(this);
-		this.factoryView = sap.ui.view({
-			id : "idFactoryView",
-			viewName : "airbus.mes.linetracker.FactoryView",
-			type : sap.ui.core.mvc.ViewType.XML,
-			height : "98%",
-			width: "100%"
-		});
-		this.factoryView.setModel(sap.ui.getCore().getModel("newFactoryModel"),	"newFactoryModel");
-		this.oView = sap.ui.view({
-			id : "idMainView",
-			viewName : "airbus.mes.linetracker.MainView",
-			type : "XML",
-		})
+    if (airbus.mes.linetracker.oView === undefined) {
+        airbus.mes.linetracker.util.RoleManager.init(this);
+        airbus.mes.linetracker.util.ModelManager.init(this);
+        this.factoryView = sap.ui.view({
+            id : "idFactoryView",
+            viewName : "airbus.mes.linetracker.FactoryView",
+            type : sap.ui.core.mvc.ViewType.XML,
+            height : "98%",
+            width: "100%"
+        });
+        this.factoryView.setModel(sap.ui.getCore().getModel("newFactoryModel"),    "newFactoryModel");
+        this.oView = sap.ui.view({
+            id : "idMainView",
+            viewName : "airbus.mes.linetracker.MainView",
+            type : "XML",
+        })
 
-		var i18nModel = new sap.ui.model.resource.ResourceModel({
-	        bundleUrl : "../components/linetracker/i18n/messageBundle.properties",
-//	        bundleLocale : "en" automatic defined by parameter sap-language
-	     });
-		
-		this.oView.setModel(i18nModel, "messageBundle");		
-//		this.oView.setModel(sap.ui.getCore().getModel("newStationModel"),	"newStationModel");
-//		this.oView.setModel(sap.ui.getCore().getModel("newFactoryModel"),	"newFactoryModel");
-		this.oView.setModel(sap.ui.getCore().getModel("newStationModel"),	"newStationModel");
-		this.oView.setModel(sap.ui.getCore().getModel("newProductionModel"), "newProductionModel");	
-		this.oView.setModel(sap.ui.getCore().getModel("PulseModel"), "PulseModel");	
-		airbus.mes.linetracker.oView = this.oView;
-		  
-		return this.oView;
-		
+        var i18nModel = new sap.ui.model.resource.ResourceModel({
+            bundleUrl : "../components/linetracker/i18n/i18n.properties",
+//            bundleLocale : "en" automatic defined by parameter sap-language
+         });
 
-	} else {
-		return airbus.mes.linetracker.oView;
-	}
+        this.oView.setModel(i18nModel, "linetrackerI18n");
+//        this.oView.setModel(sap.ui.getCore().getModel("newStationModel"),    "newStationModel");
+//        this.oView.setModel(sap.ui.getCore().getModel("newFactoryModel"),    "newFactoryModel");
+        this.oView.setModel(sap.ui.getCore().getModel("newStationModel"),    "newStationModel");
+        this.oView.setModel(sap.ui.getCore().getModel("newProductionModel"), "newProductionModel");
+        this.oView.setModel(sap.ui.getCore().getModel("PulseModel"), "PulseModel");
+        airbus.mes.linetracker.oView = this.oView;
+
+        return this.oView;
+
+
+    } else {
+        return airbus.mes.linetracker.oView;
+    }
 };
--- MES/WebContent/components/linetracker/StationView.controller.js
+++ MES/WebContent/components/linetracker/StationView.controller.js
@@ -1,67 +1,67 @@
 "use strict";
 sap.ui.controller("airbus.mes.linetracker.StationView", {
 
-	/**
-	 * Called when a controller is instantiated and its View controls (if
-	 * available) are already created. Can be used to modify the View before it
-	 * is displayed, to bind event handlers and do other one-time
-	 * initialization.
-	 * 
-	 * @memberOf airbus.StationView
-	 */
-	/*onInit : function() {		
-	},*/
-	lockStation : function(oEvent) {
-		
-		var oVBox = this.getView().byId("staionVbox2");
-		var items = oVBox.getItems();
-		for( var i = 0;i<3;i++){
-			items[i].removeStyleClass("hboxselect")
-		}
-		
-		if(oEvent){
-			
-			oEvent.getSource().addStyleClass("hboxselect");
-			airbus.mes.linetracker.util.ModelManager.line_number = parseInt(oEvent.getSource().getBindingContext("newStationModel").getPath().split("")[1],10)+1;
-			sap.ui.getCore().byId('idMainView--idProduction').setText("PRODUCTION LINE " + airbus.mes.linetracker.util.ModelManager.line_number);
-			airbus.mes.linetracker.util.ModelManager.msn = oEvent.getSource().getContent().getItems()[0].getMsn();
-			airbus.mes.linetracker.util.ModelManager.hand = oEvent.getSource().getContent().getItems()[0].getHand();
-		} else {
-			sap.ui.getCore().byId("idStationView--staionVbox2").getItems()[airbus.mes.linetracker.util.ModelManager.line_number-1].addStyleClass("hboxselect");
-		}
-	},
-	onProductionSelect:function(oEvt){
-		var oEventBus = sap.ui.getCore().getEventBus();
-		if(oEvt.getSource().getText().substring(8,10).trim()!="5"){
-		airbus.mes.linetracker.util.ModelManager.line_number = sap.ui.getCore().getModel("newStationModel").getProperty(oEvt.getSource().getBindingContext("newStationModel").getPath()).Line
-		oEventBus.publish("MainView", "onClickLine",null);
-		}
-	},
-	refreshStationDetail : function(ok){
-		airbus.mes.linetracker.util.ModelManager.loadModelStationModel();
-		airbus.mes.linetracker.util.ModelManager.refreshFactoryModel();
-		airbus.mes.linetracker.util.ModelManager.loadModelProductionModel();
+    /**
+     * Called when a controller is instantiated and its View controls (if
+     * available) are already created. Can be used to modify the View before it
+     * is displayed, to bind event handlers and do other one-time
+     * initialization.
+     *
+     * @memberOf airbus.StationView
+     */
+    /*onInit : function() {
+    },*/
+    lockStation : function(oEvent) {
 
-	},
-	
-	onStationPress : function(oEvt){
-		var sLineNumber = parseInt(oEvt.getSource().getLineNumber(),10);
-		var sMsn = oEvt.getSource().getMsn();
-		var sStationNumber = airbus.mes.linetracker.util.ModelManager.station_number;
-		var sFactory = airbus.mes.linetracker.util.ModelManager.factory_name;
-		var sSite = airbus.mes.linetracker.util.ModelManager.site;
-		
-		airbus.mes.settings.ModelManager.msn = sMsn;
-		airbus.mes.linetracker.util.ModelManager.getTranscoStation(sSite, sFactory, sLineNumber, sStationNumber);
-	},
-	/*onAfterRendering: function(){
-	}*/
+        var oVBox = this.getView().byId("staionVbox2");
+        var items = oVBox.getItems();
+        for( var i = 0;i<3;i++){
+            items[i].removeStyleClass("hboxselect")
+        }
+
+        if(oEvent){
+
+            oEvent.getSource().addStyleClass("hboxselect");
+            airbus.mes.linetracker.util.ModelManager.line_number = parseInt(oEvent.getSource().getBindingContext("newStationModel").getPath().split("")[1],10)+1;
+            sap.ui.getCore().byId('idMainView--idProduction').setText(airbus.mes.linetracker.oView.getModel("linetrackerI18n").getProperty("productionLine") + airbus.mes.linetracker.util.ModelManager.line_number);
+            airbus.mes.linetracker.util.ModelManager.msn = oEvent.getSource().getContent().getItems()[0].getMsn();
+            airbus.mes.linetracker.util.ModelManager.hand = oEvent.getSource().getContent().getItems()[0].getHand();
+        } else {
+            sap.ui.getCore().byId("idStationView--staionVbox2").getItems()[airbus.mes.linetracker.util.ModelManager.line_number-1].addStyleClass("hboxselect");
+        }
+    },
+    onProductionSelect:function(oEvt){
+        var oEventBus = sap.ui.getCore().getEventBus();
+        if(oEvt.getSource().getText().substring(8,10).trim()!="5"){
+        airbus.mes.linetracker.util.ModelManager.line_number = sap.ui.getCore().getModel("newStationModel").getProperty(oEvt.getSource().getBindingContext("newStationModel").getPath()).Line
+        oEventBus.publish("MainView", "onClickLine",null);
+        }
+    },
+    refreshStationDetail : function(ok){
+        airbus.mes.linetracker.util.ModelManager.loadModelStationModel();
+        airbus.mes.linetracker.util.ModelManager.refreshFactoryModel();
+        airbus.mes.linetracker.util.ModelManager.loadModelProductionModel();
+
+    },
+
+    onStationPress : function(oEvt){
+        var sLineNumber = parseInt(oEvt.getSource().getLineNumber(),10);
+        var sMsn = oEvt.getSource().getMsn();
+        var sStationNumber = airbus.mes.linetracker.util.ModelManager.station_number;
+        var sFactory = airbus.mes.linetracker.util.ModelManager.factory_name;
+        var sSite = airbus.mes.linetracker.util.ModelManager.site;
+
+        airbus.mes.settings.ModelManager.msn = sMsn;
+        airbus.mes.linetracker.util.ModelManager.getTranscoStation(sSite, sFactory, sLineNumber, sStationNumber);
+    },
+    /*onAfterRendering: function(){
+    }*/
 
 /**
  * Similar to onAfterRendering, but this hook is invoked before the controller's
  * View is re-rendered (NOT before the first rendering! onInit() is used for
  * that one!).
- * 
+ *
  * @memberOf airbus.StationView
  */
 // onBeforeRendering: function() {
@@ -71,13 +71,13 @@ sap.ui.controller("airbus.mes.linetracker.StationView", {
  * Called when the View has been rendered (so its HTML is part of the document).
  * Post-rendering manipulations of the HTML could be done here. This hook is the
  * same one that SAPUI5 controls get after being rendered.
- * 
+ *
  * @memberOf airbus.StationView
  */
 /**
  * Called when the Controller is destroyed. Use this one to free resources and
  * finalize activities.
- * 
+ *
  * @memberOf airbus.StationView
  */
 // onExit: function() {
