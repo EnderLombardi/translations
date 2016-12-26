a00b3033e0e486e086e8ba912cddb10e841d4dbe
Vaibhav Mishra <vaibhav.mishra@soprasteria.com>
2016-12-26 11:45:58 +0530

    [linetracker] component.js added

---
+++ MES/WebContent/components/linetracker/Component.js
@@ -0,0 +1,58 @@
+"use strict";
+
+jQuery.sap.require("sap.ui.core.UIComponent");
+jQuery.sap.require("sap.ui.base.Event");
+jQuery.sap.require("airbus.mes.linetracker.util.ModelManager");
+jQuery.sap.require("airbus.mes.linetracker.util.Formatter");
+jQuery.sap.require("airbus.mes.linetracker.util.RoleManager");
+// Declare the current Component
+jQuery.sap.declare("airbus.mes.linetracker.Component");
+
+// Extend current Component
+sap.ui.core.UIComponent.extend("airbus.mes.linetracker.Component", {
+    //manifestUrl : "Component.json",
+    metadata : {
+
+        properties : {
+//            textButtonTo : "string",
+//            buttonAction : "string"
+        }
+    }
+
+});
+
+// override the createContent function to return user interface
+airbus.mes.linetracker.Component.prototype.createContent = function() {
+
+    if (airbus.mes.linetracker.oView === undefined) {
+       /* airbus.mes.linetracker.util.RoleManager.init(this);
+        airbus.mes.linetracker.util.ModelManager.init(this);*/
+        /*this.factoryView = sap.ui.view({
+            id : "idFactoryView",
+            viewName : "airbus.mes.linetracker.FactoryView",
+            type : sap.ui.core.mvc.ViewType.XML,
+            height : "98%",
+            width: "100%"
+        });
+        this.factoryView.setModel(sap.ui.getCore().getModel("newFactoryModel"),    "newFactoryModel");
+        this.oView = sap.ui.view({
+            id : "idMainView",
+            viewName : "airbus.mes.linetracker.MainView",
+            type : "XML",
+        })*/
+
+        var i18nModel = new sap.ui.model.resource.ResourceModel({
+            bundleUrl : "../components/linetracker/i18n/i18n.properties",
+         });
+
+        this.oView.setModel(i18nModel, "i18n");
+
+        airbus.mes.linetracker.oView = this.oView;
+
+        return this.oView;
+
+
+    } else {
+        return airbus.mes.linetracker.oView;
+    }
+};
