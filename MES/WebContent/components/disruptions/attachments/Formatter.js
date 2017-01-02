81fb4eb3d6c80bc2ef894695cdb0824c3ae3cffe
Sanjo Thomas <sanjo.thomas@soprasteria.com>
2017-01-02 18:10:28 +0530

    [attachment]: disruptions
    Gravity&status: formatter
    select: initial current disruption
    data binding acc to disruption

---
+++ MES/WebContent/components/disruptions/attachments/Formatter.js
@@ -0,0 +1,51 @@
+"use strict";
+
+jQuery.sap.declare("airbus.mes.disruptions.attachments.Formatter");
+
+airbus.mes.disruptions.attachments.Formatter = {
+
+	status : {
+		"pending" 	: "Pending",
+		"closed" 	: "Closed",
+		"acknowledged" : "Acknowledged",
+		"solved" 	: "Solved",
+		"rejected" 	: "Rejected",
+		"deleted" 	: "Deleted"
+	},
+		
+	setGravityTexts : function(gravity) {
+		
+		var property;
+		
+		switch(gravity) {
+		case "1":
+			property = airbus.mes.disruptions.attachments.oView.getModel("i18n").getProperty("NotBlocked");
+			break;
+		case "2":
+			property = airbus.mes.disruptions.attachments.oView.getModel("i18n").getProperty("Disturbed");
+			break;
+		case "3":
+			property = airbus.mes.disruptions.attachments.oView.getModel("i18n").getProperty("Blocked");
+			break;
+		default:
+			break;
+		}
+		return property;
+	},
+
+
+	
+	setDisruptionStatuses : function(status){
+		return airbus.mes.disruptions.attachments.oView.getModel("i18n").getProperty(status);
+	},
+	isStatusFinal : function(status) {
+		if( status === airbus.mes.disruptions.Formatter.status.closed 
+	    ||  status === airbus.mes.disruptions.Formatter.status.solved
+	    ||  status === airbus.mes.disruptions.Formatter.status.rejected 
+	    ||  status === airbus.mes.disruptions.Formatter.status.deleted	){
+			return true;
+		} else {
+			return false;
+		}
+	}
+};
