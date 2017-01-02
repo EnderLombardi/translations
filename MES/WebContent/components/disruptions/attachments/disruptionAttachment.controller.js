c7270dccb8e6623df1d6146cb896a0ca989202d3
Sanjo Thomas <sanjo.thomas@soprasteria.com>
2017-01-02 12:13:17 +0530

    [attachment]:Disruption Update

--- MES/WebContent/components/disruptions/attachments/disruptionAttachment.controller.js
+++ MES/WebContent/components/disruptions/attachments/disruptionAttachment.controller.js
@@ -48,15 +48,6 @@ sap.ui.define([
 				aFilter.push(new Filter("dis_des", FilterOperator.Contains, oCurrDisVal));	
 				}
 			oBinding.filter(aFilter);
-			// var loSelectState = this.getView().byId("idshipstate");
-			// var loStatePath = "dealer>" + "/CountrySet('"+loCountry+"')/Region";
-			// loSelectState.bindItems({
-			// 	path: loStatePath,
-			// 	template: new sap.ui.core.Item({
-			// 		text:"{dealer>Statename}",
-			// 		key:"{dealer>State}"
-			// 	})
-			// });
 			
 		},
 		onSelectAll: function(){
@@ -67,143 +58,13 @@ sap.ui.define([
 			aFilter.push(new Filter("wo_no", FilterOperator.Contains, oCurrWo_noVal));
 			oBinding.filter(aFilter);
 		},
+		onPressDelete: function(oEvt){
+			 var loPath = oEvt.oSource.oPropagatedProperties.oBindingContexts.attachDisruption.sPath;
+			 var loLength = loPath.length;
+			 var loindex = loPath.slice(loLength-1);
+		},
 		
-//		onInit: function(){
-//			var loHeaderPath = "attachDisruption" + ">/"
-//			var loDisruptionList = this.getView().byId("idDisruptionsList")
-//			var list = this.getView().byId("idDisruptionsist")
-//
-//			loDisruptionList.bindItems({
-//				path: "attachDisruption>/Rowsets/Rowset/0/Row/0/wp01",
-////				sorter: new sap.ui.model.Sorter("attachDisruption>FirstName", false, function(oContext) {
-////					return {
-////						key: oContext.getProperty("NameChar")
-////					};
-////				}),
-////				template: new sap.m.ObjectListItem({
-////					title: "{attachDisruption>wo_no} {attachDisruption>op_no}",
-////					attributes: [new sap.m.ObjectAttribute({
-////						text: "{attachDisruption>dis_no}"
-////					}), new sap.m.ObjectAttribute({
-////						text: "{attachDisruption>dis_des}"
-////					}), new sap.m.ObjectAttribute({
-////						text: "{attachDisruption>gravity}"
-////					})],
-////					firstStatus: new sap.m.ObjectStatus({
-////						text: "{attachDisruption>status}"
-////					})
-////
-////				})
-//				
-//				
-//				
-//				
-//				
-//				
-////				template: new sap.m.CustomListItem({
-////				    content: [
-////				            new sap.m.Panel({
-////				            	expandable:true,
-//////						headerText : "This is the Panel header",
-////						headerToolbar : new sap.m.Toolbar({
-////							content : [
-////							new sap.m.Title({
-////								text : "{attachDisruption>wo_no}-{attachDisruption>op_no}-{attachDisruption>dis_no}-{attachDisruption>dis_des}-{attachDisruption>gravity}-{attachDisruption>status}"
-////							})
-////							]
-////						}),
-////						content : [
-////						           list.bindItems({
-////						        	   path: "attachDisruption>Row1/",
-////						        	 template:  new sap.m.CustomListItem({
-////							        	   content: [
-////											            new sap.m.Panel({
-////											            	expandable:true,
-////													headerToolbar : new sap.m.Toolbar({
-////														content : [
-////														new sap.m.Title({
-////															text : "{attachDisruption>Title}"
-////														})
-////														]
-////													})
-////											            })
-////											            ]
-////							           })
-////						           })
-////						           ],
-////				            
-////				    })
-////						],
-////				})				
-//				
-//				
-//				template: new sap.m.CustomListItem({
-//				    content: [
-//				            new sap.m.Panel({
-//				            	expandable:true,
-//				            	expand: function(oEvt){
-//				            		var oPath = oEvt.oSource.oPropagatedProperties.oBindingContexts.attachDisruption.sPath;
-//				            		var oLength = oPath.length;
-//				            		var oIndex = oPath.slice(oLength-1);
-//				            		var oList = this.oParent.oParent.oParent.byId("idDisruptionsist");
-//				            		oList.bindItems({
-//
-//				        		        	   path: "attachDisruption>/Rowsets/Rowset/0/Row/0/wp01/0/op01/0/dp01/"+oIndex+"/",
-//				        		        	 template:  new sap.m.CustomListItem({
-//				        			        	   content: [
-//				        							            new sap.m.Panel({
-//				        							            	expandable:true,
-//				        									headerToolbar : new sap.m.Toolbar({
-//				        										content : [
-//				        										new sap.m.Title({
-//				        											text : "{attachDisruption>dis_no}"
-//				        										})
-//				        										]
-//				        									})
-//				        							            })
-//				        							            ]
-//				        			           })
-//				        		});
-//				            		var oModel = oList.getBindingContext().getModel();
-//				            		var oContext = oModel.getContext(sPath);
-//				            		oList.setBindingContext(oContext);
-//				            		
-//				            		
-//				            	},
-////						headerText : "This is the Panel header",
-//						headerToolbar : new sap.m.Toolbar({
-//							content : [
-//							new sap.m.Title({
-//								text : "{attachDisruption>wo_no}{attachDisruption>op01/0/op_no}"
-//							})
-//							]
-//						}),
-//						content : [
-//						           list.bindItems({
-//						        	   path: "attachDisruption>/Rowsets/Rowset/0/Row/0/wp01/0/op01/0/dp01/",
-//						        	 template:  new sap.m.CustomListItem({
-//							        	   content: [
-//											            new sap.m.Panel({
-//											            	expandable:true,
-//													headerToolbar : new sap.m.Toolbar({
-//														content : [
-//														new sap.m.Title({
-//															text : "{attachDisruption>dis_no}"
-//														})
-//														]
-//													})
-//											            })
-//											            ]
-//							           })
-//						           })
-//						           ],
-//				            
-//				    })
-//						],
-//				})
-//
-//			});
-//		},
+
 		onNavPress: function(){
 			this.nav = this.getView().oParent;
 			this.nav.back();
