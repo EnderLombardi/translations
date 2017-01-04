394285e83dac24eeb38d746ab703860a83a09fc2
Sanjo Thomas <sanjo.thomas@soprasteria.com>
2017-01-04 16:24:27 +0530

    [attachment]
    commented code for filtering:
    
    Select in disruptionattachment view
    before rendering and on filtering in disruption controller

--- MES/WebContent/components/disruptions/attachments/disruptionAttachment.controller.js
+++ MES/WebContent/components/disruptions/attachments/disruptionAttachment.controller.js
@@ -13,69 +13,69 @@ sap.ui.define([
 
 
 		
-		onBeforeRendering: function(oEvt){
-			var aFilter = [];
-			var that = this;
-			this.getView().addEventDelegate({
-				onAfterShow: function(evt) {
-				      var oCurrDisVal = evt.data.Desc;
-				      
-				      var oList = that.getView().byId("idList");
-						var oBinding = oList.getBinding("items");
-//						var oCurrDisVal = sap.ui.getCore().getModel("operationDisruptionsModel").oData.Rowsets.Rowset[0].Row[0].Description;
-							aFilter.push(new Filter("dis_des", FilterOperator.Contains, oCurrDisVal));	
-							
-						oBinding.filter(aFilter);
-				      // ...now retrieve the data element with the given ID and update the page UI
-				   },
-				   
-				});
-
-		      
-//			var idToRetrieve = evt.data.Desc;
-			
-		},
-		onFilterChange: function(oEvent){
-			var loValue = oEvent.getSource().getSelectedKey();
-			
-			
-			if (loValue === "Current Disruption"){
-				if(this.getView().byId("idCheckBox").getSelected() === true){
-					this.getView().byId("idCheckBox").setSelected(false)
-				}
-//			var oCurrDisVal = "Disruption 1";
-			var oCurrDisVal = sap.ui.getCore().getModel("operationDisruptionsModel").oData.Rowsets.Rowset[0].Row[0].Description
-			}
-			if (loValue==="Current Work Order"){
-				if(this.getView().byId("idCheckBox").getSelected() === true){
-					this.getView().byId("idCheckBox").setSelected(false)
-				}
-//			var oCurrWo_noVal = "26557";
-			var oCurrWo_noVal = sap.ui.getCore().getModel("operationDetailModel").oData.Rowsets.Rowset[0].Row[0].wo_no;	
-			}
-			if (loValue === "Current Operation"){
-				if(this.getView().byId("idCheckBox").getSelected() === true){
-					this.getView().byId("idCheckBox").setSelected(false)
-				}
-//			var oCurrop_noVal= "50002137-1-0-0011";
-			var oCurrop_noVal = sap.ui.getCore().getModel("operationDetailModel").oData.Rowsets.Rowset[0].Row[0].operation_no;	
-			}
-			
-			var aFilter = [];
-			var oList = this.getView().byId("idList");
-			var oBinding = oList.getBinding("items");
-				if(oCurrop_noVal){
-				aFilter.push(new Filter("op_no", FilterOperator.Contains, oCurrop_noVal));
-				}
-				if(oCurrWo_noVal){
-				aFilter.push(new Filter("wo_no", FilterOperator.Contains, oCurrWo_noVal));
-				}
-				if(oCurrDisVal){
-				aFilter.push(new Filter("dis_des", FilterOperator.Contains, oCurrDisVal));	
-				}
-			oBinding.filter(aFilter);
-			
-		},
+//		onBeforeRendering: function(oEvt){
+//			var aFilter = [];
+//			var that = this;
+//			this.getView().addEventDelegate({
+//				onAfterShow: function(evt) {
+//				      var oCurrDisVal = evt.data.Desc;
+//				      
+//				      var oList = that.getView().byId("idList");
+//						var oBinding = oList.getBinding("items");
+////						var oCurrDisVal = sap.ui.getCore().getModel("operationDisruptionsModel").oData.Rowsets.Rowset[0].Row[0].Description;
+//							aFilter.push(new Filter("dis_des", FilterOperator.Contains, oCurrDisVal));	
+//							
+//						oBinding.filter(aFilter);
+//				      // ...now retrieve the data element with the given ID and update the page UI
+//				   },
+//				   
+//				});
+//
+//		      
+////			var idToRetrieve = evt.data.Desc;
+//			
+//		},
+//		onFilterChange: function(oEvent){
+//			var loValue = oEvent.getSource().getSelectedKey();
+//			
+//			
+//			if (loValue === "Current Disruption"){
+//				if(this.getView().byId("idCheckBox").getSelected() === true){
+//					this.getView().byId("idCheckBox").setSelected(false)
+//				}
+////			var oCurrDisVal = "Disruption 1";
+//			var oCurrDisVal = sap.ui.getCore().getModel("operationDisruptionsModel").oData.Rowsets.Rowset[0].Row[0].Description
+//			}
+//			if (loValue==="Current Work Order"){
+//				if(this.getView().byId("idCheckBox").getSelected() === true){
+//					this.getView().byId("idCheckBox").setSelected(false)
+//				}
+////			var oCurrWo_noVal = "26557";
+//			var oCurrWo_noVal = sap.ui.getCore().getModel("operationDetailModel").oData.Rowsets.Rowset[0].Row[0].wo_no;	
+//			}
+//			if (loValue === "Current Operation"){
+//				if(this.getView().byId("idCheckBox").getSelected() === true){
+//					this.getView().byId("idCheckBox").setSelected(false)
+//				}
+////			var oCurrop_noVal= "50002137-1-0-0011";
+//			var oCurrop_noVal = sap.ui.getCore().getModel("operationDetailModel").oData.Rowsets.Rowset[0].Row[0].operation_no;	
+//			}
+//			
+//			var aFilter = [];
+//			var oList = this.getView().byId("idList");
+//			var oBinding = oList.getBinding("items");
+//				if(oCurrop_noVal){
+//				aFilter.push(new Filter("op_no", FilterOperator.Contains, oCurrop_noVal));
+//				}
+//				if(oCurrWo_noVal){
+//				aFilter.push(new Filter("wo_no", FilterOperator.Contains, oCurrWo_noVal));
+//				}
+//				if(oCurrDisVal){
+//				aFilter.push(new Filter("dis_des", FilterOperator.Contains, oCurrDisVal));	
+//				}
+//			oBinding.filter(aFilter);
+//			
+//		},
 		onSelectAll: function(){
 			var aFilter = [];
 			var oCurrWo_noVal = sap.ui.getCore().getModel("operationDetailModel").oData.Rowsets.Rowset[0].Row[0].wo_no;	
