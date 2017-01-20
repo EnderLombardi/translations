926899fdd8bdc7cae55e6433194fe68e66400b48
vinay singh <vinay.singh@soprasteria.com>
2017-01-20 16:52:18 +0530

    [disruptionTracker]: Export disruption To excel file added

--- MES/WebContent/components/disruptiontracker/disruptions.controller.js
+++ MES/WebContent/components/disruptiontracker/disruptions.controller.js
@@ -427,6 +427,78 @@ sap.ui.controller("airbus.mes.disruptiontracker.disruptions", {
 
 	},
 	/**
+	 * Export Disruption Data to excel
+	 * [MES V1.5]
+	 * [SD-SP1604983-EXT-005]
+	 */
+	handleSelectedRowExcelExport:function(){
+		var oTable = this.getView().byId("disruptionsTable");
+		var aContexts = oTable.getSelectedContexts();
+		var aItems = aContexts.map(function(oEvent){
+		        return oEvent.getObject();
+		    });
+		this.jsonToCSVConvertor(aItems, "Disruption Data", true);
+		
+	},
+	jsonToCSVConvertor: function(JSONData, ReportTitle, ShowLabel){
+
+        //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
+        var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+        var CSV = '';    
+        //This condition will generate the Label/Header
+        if (ShowLabel) {
+            var row = "";
+
+            //This loop will extract the label from 1st index of on array
+            for (var index in arrData[0]) {
+                //Now convert each value to string and comma-seprated
+                row += index + ',';
+            }
+            row = row.slice(0, -1);
+            //append Label row with line break
+            CSV += row + '\r\n';
+        }
+
+        //1st loop is to extract each row
+        for (var i = 0; i < arrData.length; i++) {
+            var row = "";
+            //2nd loop will extract each column and convert it in string comma-seprated
+            for (var index in arrData[i]) {
+                row += '"' + arrData[i][index] + '",';
+            }
+            row.slice(0, row.length - 1);
+            //add a line break after each row
+            CSV += row + '\r\n';
+        }
+
+        if (CSV == '') {        
+            alert("Invalid data");
+            return;
+        }   
+
+        //this trick will generate a temp "a" tag
+        var link = document.createElement("a");    
+        link.id="lnkDwnldLnk";
+
+        //this part will append the anchor tag and remove it after automatic click
+        document.body.appendChild(link);
+
+        var csv = CSV;  
+        var blob = new Blob([csv], { type: 'text/csv' }); 
+        var csvUrl = window.URL.createObjectURL(blob);
+        var filename = 'UserExport.csv';
+        $("#lnkDwnldLnk")
+        .attr({
+            'download': filename,
+            'href': csvUrl
+        }); 
+
+        $('#lnkDwnldLnk')[0].click();    
+        document.body.removeChild(link);
+
+    }
+
+	/**
 	 * search disruption on basis of work order/operation
 	 * 
 	 * @param {object}
