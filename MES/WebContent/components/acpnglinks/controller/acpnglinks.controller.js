717d91cfc9c227efbb613211c26bcede1661c97f
Valentin Vincent <valentin.vincent2@soprasteria.com>
2017-01-30 18:11:06 +0100

    [acpnglinks] work on new view to select coumns

--- MES/WebContent/components/acpnglinks/controller/acpnglinks.controller.js
+++ MES/WebContent/components/acpnglinks/controller/acpnglinks.controller.js
@@ -4,25 +4,30 @@ sap.ui.controller("airbus.mes.acpnglinks.controller.acpnglinks", {
 	onAfterRendering : function() {
 		var oTTbl = sap.ui.getCore().byId("acpnglinksView--ACPnGTreeTable");
 		if (oTTbl != undefined){
-			this.treeExpandAll(oTTbl);
-			oTTbl.setVisibleRowCount(oTTbl._getRowCount());
+			
+			oTTbl.setVisibleRowCount(this.treeExpandAll(oTTbl));
 			oTTbl.setSelectionMode(sap.ui.table.SelectionMode.Single);
 		}
         
     },
     
     treeExpandAll : function(oTTbl) {  
-	        for (var i=0; i<oTTbl._getRowCount(); i++){
+	        for (var i=0; i<oTTbl.getRows().length; i++){
 				try{
 		            oTTbl.expand(i);
 	        	} catch(exception) {
-	        		//do nothing
+	        		return i;
 	        	}
-       }
+	        }
+	        return oTTbl.getRows().length;
     },
     
     OnSelectionChange : function(oEvt){
+    	try{
     	airbus.mes.acpnglinks.oView.getModel("acpnglinksWorkOrderDetail").getProperty(oEvt.mParameters.rowBindingContext.sPath);
     	console.log(oEvt.getSource());
+    	} catch(exception) {
+    		//do nothing
+    	}
     }
 });
