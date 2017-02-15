6300101ea693457744e5e458cc1e31017f39997d
Nicolas Janvier <nicolas.janvier@soprasteria.com>
2017-02-15 17:49:44 +0100

    [Polypoly] Fixed authorizations breaking build of Polypoly

--- MES/WebContent/components/polypoly/polypoly.controller.js
+++ MES/WebContent/components/polypoly/polypoly.controller.js
@@ -566,12 +566,15 @@ sap.ui.controller("airbus.mes.polypoly.polypoly",{
                                                                                      || type == "UA_P"
                                                                              }
                                                                          },
-                                                                         enabled : function(){
-                                                                 	    	if(sap.ui.getCore().getModel("Profile").oData.connectedUser.permissions.PM_POLY_ASSESS === true ){
-                                                                         	return true;
-                                                                         	} else { 
-                                                                         	return false;
-                                                                         	} 
+                                                                         enabled : {
+                                                                             parts : [ "type" ],
+                                                                             formatter : function(){
+                                                                      	    	if(sap.ui.getCore().getModel("Profile").oData.connectedUser.permissions.PM_POLY_ASSESS === true ){
+                                                                                 	return true;
+                                                                                 	} else { 
+                                                                                 	return false;
+                                                                                 	} 
+                                                                                 }
                                                                          },
                                                                          press : function(
                                                                                  oEvt) {
