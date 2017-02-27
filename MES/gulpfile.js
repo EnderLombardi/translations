af925a5ce13cee51dc04df84812968cc2971a83c
Delphin Barraud <delphin.barraud@soprasteria.com>
2017-02-27 19:06:02 +0100

    [BUMP] Push new CSS file for print.

--- MES/gulpfile.js
+++ MES/gulpfile.js
@@ -141,7 +141,8 @@ gulp.task('copy', ['clean'], function () {
 		'./components/stationtracker/data/KPIModel.json',
 		'./components/disruptions/local/Jigtool_Server.json',
 		'./components/disruptions/local/MaterialList_Server.json',
-		'./components/components/data/selectFilterModel.json'
+		'./components/components/data/selectFilterModel.json',
+		'./components/trackingtemplate/styles/trackingtemplatePrint.css'
 	], { cwd: src, cwdbase: true }).pipe(gulp.dest(dest));
 });
 
