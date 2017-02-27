2114d61e82ee17c4a7b764bf5d60049480a5e66d
Jean-Philippe Kha <jean-philippe.kha@soprasteria.com>
2017-02-27 16:26:58 +0100

    [trackingtemplate] resolve bug operation number

--- MES/WebContent/components/trackingtemplate/util/ModelManager.js
+++ MES/WebContent/components/trackingtemplate/util/ModelManager.js
@@ -83,93 +83,41 @@ airbus.mes.trackingtemplate.util.ModelManager = {
         });
     },
 
+
     /**
-     * sort array by created date time
+     * getOperationNumber
      */
-    sortByCreatedDateTime: function (array) {
-        array.sort(function (a, b) {
-            if (a.Created_Date_Time < b.Created_Date_Time) {
-                return 1;
-            }
-            return 0;
-        });
+
+    getOperationNumber: function (row) {
+        row["User_First_Name"] = row["User_First_Name"].substring(0, 1);
+        var regex = regex = /-([^,]*)/;
+        if (regex.exec(row.OperationBO)) {
+            var arrayOperationNumber = regex.exec(row.OperationBO)[1].split('-'); //return smth like : "1-0-0010" we split to get the last number array of length 3 here
+            var operationNumber = arrayOperationNumber[arrayOperationNumber.length - 1]; //get the last number
+            return operationNumber || '';
+        }
     },
 
-    /**
-     * we order the array by grouping the items by operation (beginning string of Production_Context_GBO)
-     * we place ShopOrderBo at the top of the array
-    */
-    sortByOperation: function (array, regex) {
-        array.sort(function (a, b) {
-            if (a.Production_Context_GBO === b.Production_Context_GBO) {
-                return 0;
-            };
-            if (b.Production_Context_GBO.toUpperCase().startsWith("SHOPORDERBO")) {
-                return 2;
-            }
-            if (a.Production_Context_GBO.toUpperCase().startsWith("SHOPORDERBO")) {
-                return -2;
-            }
-
-            if (regex.exec(a.OperationBO) && regex.exec(b.OperationBO)) {
-                if (regex.exec(a.OperationBO)[1] < regex.exec(b.OperationBO)[1]) {
-                    return -1;
-                }
-                if (regex.exec(a.OperationBO)[1] > regex.exec(b.OperationBO)[1]) {
-
-                    return 1;
-                }
-            }
-            return 0;
-        });
-    },
     /**
      * sort Array by operation and date
      */
     sortArrayByOperationAndDate: function (array) {
 
-        var index, len, previousRow, regex;
-        // regex in order to catch the operation number
-        regex = /-([^,]*)/;
-        // //we order the array by Created_Date_Time
-        // this.sortByCreatedDateTime(array);
-        // //we order the array by grouping the items by operation (beginning string of Production_Context_GBO)
-        // //we place ShopOrderBo at the top of the array
-        // this.sortByOperation(array, regex);
-        
-        array = array.sort(airbus.mes.shell.util.Formatter.fieldComparator([ 'OperationBO', '-Created_Date_Time' ]));
-
-
+        var index, len, previousRow;
+        array = array.sort(airbus.mes.shell.util.Formatter.fieldComparator(['OperationBO', '-Created_Date_Time']));
         index = 1;
         len = array.length;
         //we update the user first name to show only the first letter.
-        array[0]["User_First_Name"] = array[0]["User_First_Name"].substring(0, 1);
-        array[0].lastOperationNote = true;
         previousRow = array[0];
+        previousRow.lastOperationNote = true;
+        previousRow.operationNumber = this.getOperationNumber(previousRow);
 
-        //We sort the array in order to have ShopOrderBo at the top
-        //we need to reach to the first element which is not a ShopOrderBO
-        for (; index < len; index += 1) {
-            //we update the user first name to show only the first letter. 
-            array[index]["User_First_Name"] = array[index]["User_First_Name"].substring(0, 1);
-            if (!previousRow.Production_Context_GBO.toUpperCase().startsWith("SHOPORDERBO")) {
-                previousRow.lastOperationNote = true;
-                var arrayOperationNumber = regex.exec(previousRow.OperationBO)[1].split('-'); //return smth like : "1-0-0010" we split to get the last number array of length 3 here
-                var operationNumber = arrayOperationNumber[arrayOperationNumber.length - 1]; //get the last number
-                previousRow.operationNumber = operationNumber || '';
-                break;
-            }
-            previousRow = array[index];
-        }
         //we add the attribute lastOperationNote to each item of the array. This attribute is set to true 
         //for each most recent confirmation note of a group of operation(Production_Context_GBO)
         for (; index < len; index += 1) {
             //we update the user first name to show only the first letter. 
-            array[index]["User_First_Name"] = array[index]["User_First_Name"].substring(0, 1);
-            var arrayOperationNumber = regex.exec(array[index].OperationBO)[1].split('-'); //return smth like : "1-0-0010" we split to get the last number array of length 3 here
-            var operationNumber = arrayOperationNumber[arrayOperationNumber.length - 1]; //get the last number
-            array[index].operationNumber = operationNumber || '';
-            if (regex.exec(previousRow.OperationBO)[1] !== regex.exec(array[index].OperationBO)[1]) {
+            array[index].operationNumber = this.getOperationNumber(array[index]);
+            if (previousRow.OperationBO !== array[index].OperationBO) {
                 array[index].lastOperationNote = true;
             } else {
                 array[index].lastOperationNote = false;
@@ -177,7 +125,6 @@ airbus.mes.trackingtemplate.util.ModelManager = {
             previousRow = array[index];
         }
         return array;
-
     },
 
 
