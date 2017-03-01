b8ea5ad9c2aae70538c25a0f6034a37862400eb6
Sanjo Thomas <sanjo.thomas@soprasteria.com>
2017-03-01 19:15:40 +0530

    [qdc] updates on checklist and records

--- MES/WebContent/components/qdc/util/Formatter.js
+++ MES/WebContent/components/qdc/util/Formatter.js
@@ -4,6 +4,7 @@ jQuery.sap.declare("airbus.mes.qdc.util.Formatter");
 
 airbus.mes.qdc.util.Formatter = {
 	/**
+	 * BR: SD-QDC-HMI-130
 	 * This function is used to show the Exclamation mark symbol
 	 * from the local json model, moreover the icon src is set.
 	 *
@@ -15,6 +16,7 @@ airbus.mes.qdc.util.Formatter = {
 	},
 
 	/**
+	 * BR : SD-QDC-HMI-160
 	 * This function is used to show the QDC data on the UI, based on the 
 	 * QDC status, moreover the External tools buttons are enabled based on the status
 	 *
@@ -23,7 +25,7 @@ airbus.mes.qdc.util.Formatter = {
 		var oVal = sap.ui.getCore().getModel("GetQDCDataModel");
 
 		var obj = oVal.oData.Rowsets.Rowset[1].Row;
-		if (oVal.oData.Rowsets.Rowset[0].Row[0].QDCSTATUS === "") {
+		if (oVal.oData.Rowsets.Rowset[0].Row[0].QDCSTATUS === "X") {
 			if (airbus.mes.operationdetail.oView.byId("switchStatusLabel").mProperties.text === "Execution Mode") {
 				obj.filter(function(row) {
 					if (row.DOC_TYPE === "MEA") {
@@ -54,6 +56,7 @@ airbus.mes.qdc.util.Formatter = {
 	},
 
 	/**
+	 * BR: SD-QDC-HMI-160
 	 * This function is used to enable or disable the External tools buttons on switching 
 	 * the display only and Execution mode Switch control
 	 *
@@ -61,7 +64,7 @@ airbus.mes.qdc.util.Formatter = {
 	getButtonsEnabled : function() {
 		var oVal = sap.ui.getCore().getModel("GetQDCDataModel");
 		var obj = oVal.oData.Rowsets.Rowset[1].Row;
-		if (airbus.mes.operationdetail.oView.byId("switchStatusLabel").mProperties.text === "Execution Mode") {
+		if (airbus.mes.operationdetail.oView.byId("switchOperationModeBtn").getState() === true) {
 
 			if (oVal.oData.Rowsets.Rowset[0].Row[0].QDCSTATUS === "X") {
 
@@ -87,7 +90,7 @@ airbus.mes.qdc.util.Formatter = {
 				});
 			}
 		} else
-			(airbus.mes.operationdetail.oView.byId("switchStatusLabel").mProperties.text === "Read Only")
+			(airbus.mes.operationdetail.oView.byId("switchOperationModeBtn").getState() === false)
 		{
 			if (oVal.oData.Rowsets.Rowset[0].Row[0].QDCSTATUS === "X") {
 
@@ -116,16 +119,17 @@ airbus.mes.qdc.util.Formatter = {
 	},
 
 	/**
+	 * BR: SD-QDC-HMI-140
 	 * This function is used to display the counter
 	 * values
 	 *
 	 */
-	getDescription : function(oEvt) {
+	getDescription : function(iTotCount,iCloseCount) {
 		// var iRootLevel = this.getParent().getParent().getRootLevel();
 		if (this.getParent().getCells()[0]) {
-			var oVal = sap.ui.getCore().getModel("GetQDCDataModel");
-			var iTotCount = oVal.oData.Rowsets.Rowset[0].Row[0].TOT_COUNT;
-			var iCloseCount = oVal.oData.Rowsets.Rowset[0].Row[0].CLOSE_COUNT;
+//			var oVal = sap.ui.getCore().getModel("GetQDCDataModel");
+//			var iTotCount = oVal.oData.Rowsets.Rowset[0].Row[0].TOT_COUNT;
+//			var iCloseCount = oVal.oData.Rowsets.Rowset[0].Row[0].CLOSE_COUNT;
 
 			if (iTotCount === iCloseCount) {
 				return "";
@@ -139,14 +143,15 @@ airbus.mes.qdc.util.Formatter = {
 	},
 
 	/**
+	 * BR: SD-QDC-HMI-130
 	 * This function is used to display the orange Exclamation mark symbol
 	 * based on the TotaCount and CloseCount Difference.
 	 * If Difference is greater than 1, it would be visible.
 	 */
-	getExclamationVisible : function() {
-		var oVal = sap.ui.getCore().getModel("GetQDCDataModel");
-		var iTotCount = oVal.oData.Rowsets.Rowset[0].Row[0].TOT_COUNT;
-		var iCloseCount = oVal.oData.Rowsets.Rowset[0].Row[0].CLOSE_COUNT;
+	getExclamationVisible : function(iTotCount,iCloseCount) {
+//		var oVal = sap.ui.getCore().getModel("GetQDCDataModel");
+//		var iTotCount = oVal.oData.Rowsets.Rowset[0].Row[0].TOT_COUNT;
+//		var iCloseCount = oVal.oData.Rowsets.Rowset[0].Row[0].CLOSE_COUNT;
 
 		var iDiff = iTotCount - iCloseCount;
 
