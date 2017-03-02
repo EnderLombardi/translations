f6b7ac0e9d40979bc590ad7d5efaab0a525295e2
Barillon Pierre <pierre.barillon@soprasteria.com>
2017-03-02 18:29:27 +0100

    [hompage] roles

--- MES/WebContent/components/homepage/util/Formatter.js
+++ MES/WebContent/components/homepage/util/Formatter.js
@@ -2,21 +2,21 @@
 jQuery.sap.declare("airbus.mes.homepage.util.Formatter");
 
 airbus.mes.homepage.util.Formatter = {
-	
+
 	enabledTiles : function(sFeature) {
+
+		if (sap.ui.getCore().getModel("Profile").getProperty("/identifiedUser/permissions/HOMESCREEN")){
 		
-		if ( sap.ui.getCore().getModel("Profile").getProperty("/identifiedUser/permissions/" + sFeature) != undefined ) {
-			
-			if(sap.ui.getCore().getModel("Profile").getProperty("/identifiedUser/permissions/" + sFeature)){
-				return "true";				
-			}else {
-				return "false";
-			};
+			if (sap.ui.getCore().getModel("Profile").getProperty("/identifiedUser/permissions/" + sFeature) != undefined) {
+	
+				return sap.ui.getCore().getModel("Profile").getProperty("/identifiedUser/permissions/" + sFeature);
+	
+			}
+	
+			return true;
 			
 		} 
-		
-		return true;
-	
+		return false;
 	}
 
 };
