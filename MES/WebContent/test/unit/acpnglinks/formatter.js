9cd0ff4465ed3c351e7b956bc66f0c8f6ca6a98d
Valentin Vincent <valentin.vincent2@soprasteria.com>
2017-03-04 14:59:14 +0100

    [acpnglinks] automated unitary tests initialization

--- MES/WebContent/test/unit/acpnglinks/formatter.js
+++ MES/WebContent/test/unit/acpnglinks/formatter.js
@@ -23,7 +23,29 @@ sap.ui.require([
 			var acpnglinksFormatter = airbus.mes.acpnglinks.util.Formatter;
 
 			// Assert
-			assert.strictEqual(acpnglinksFormatter.toBooleanLeft("never"),false, "Booleanleft('never') = false");
+			//toBooleanLeft
+			assert.strictEqual(acpnglinksFormatter.toBooleanLeft("never"),false, "BooleanLeft('never') = false");
+			assert.strictEqual(acpnglinksFormatter.toBooleanLeft("true"),false, "BooleanLeft('true') = false");
+			assert.strictEqual(acpnglinksFormatter.toBooleanLeft("false"),true, "BooleanLeft('false') = true");
+			assert.strictEqual(acpnglinksFormatter.toBooleanLeft(),false, "BooleanLeft() = false");
+			
+			//toBooleanRight
+			assert.strictEqual(acpnglinksFormatter.toBooleanRight("never"),false, "BooleanRight('never') = false");
+			assert.strictEqual(acpnglinksFormatter.toBooleanRight("true"),true, "BooleanRight('true') = false");
+			assert.strictEqual(acpnglinksFormatter.toBooleanRight("false"),false, "BooleanRight('false') = true");
+			assert.strictEqual(acpnglinksFormatter.toBooleanRight(),false, "BooleanRight() = false");
+			
+			//levelFormat
+			assert.strictEqual(acpnglinksFormatter.levelFormat("A"),"1","levelFormat('A') = '1'");
+			assert.strictEqual(acpnglinksFormatter.levelFormat(),"1","levelFormat() = '1'");
+			assert.strictEqual(acpnglinksFormatter.levelFormat("0"),"0","levelFormat('0') = '0'");
+			assert.strictEqual(acpnglinksFormatter.levelFormat("5"),"0","levelFormat('5') = '0'");
+			assert.strictEqual(acpnglinksFormatter.levelFormat("15"),"0","levelFormat('15') = '0'");
+			assert.strictEqual(acpnglinksFormatter.levelFormat("1"),"1","levelFormat('1') = '1'");
+			assert.strictEqual(acpnglinksFormatter.levelFormat("6"),"1","levelFormat('6') = '1'");
+			
+			//currentWOFormat
+			
 		});
 	}
 
