c4c7c506f9011baff7ed36a8a8af9a0578fb0cb5
EMEAAD\jpkha <jean-philippe.kha@soprasteria>
2017-03-07 17:59:33 +0100

    [trackingtemplate] add number of attached document

--- MES/WebContent/components/trackingtemplate/util/Formatter.js
+++ MES/WebContent/components/trackingtemplate/util/Formatter.js
@@ -7,7 +7,13 @@ airbus.mes.trackingtemplate.util.Formatter = {
     commentFormat: function (sComment) {
         var regex = /^([^-]*-)?(.*)$/;
         return regex.exec(sComment)[2];
-    }
+    },
 
+    isEmpty: function (length) {
+        if(length === 0) {
+            return false;
+        }
+        return true;
+    }
 
 }
