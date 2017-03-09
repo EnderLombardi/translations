cea38efd2340ba4cfb56b1592a885e3c915a7db8
Pierandrea Pascolini <pierandrea.pascolini@soprasteria.com>
2017-03-09 14:46:43 +0100

    [settings]sort the comboboxes
    + indent calendar view

--- MES/WebContent/components/settings/util/Formatter.js
+++ MES/WebContent/components/settings/util/Formatter.js
@@ -4,56 +4,33 @@ jQuery.sap.declare("airbus.mes.settings.util.Formatter");
 
 airbus.mes.settings.util.Formatter = {
 
-	enabledProgram : function(sValue) {
-
+	enabledProgram: function (sValue) {
 		var oModel = sap.ui.getCore().getModel("plantModel");
-
 		if (oModel.getProperty("/Rowsets/Rowset/0/Row")) {
-
 			oModel = sap.ui.getCore().getModel("plantModel").oData.Rowsets.Rowset[0].Row;
-
 		} else {
 			oModel = []
-			console.log("no plantModel load");
 		}
 
-		if (oModel.map(function(x) {
-			return x.program;
-		}).indexOf(sValue) != -1) {
-
+		if (oModel.map(function (x) { return x.program; }).indexOf(sValue) != -1) {
 			return true;
-
 		} else {
-
 			return false;
 		}
-
 	},
 
-	enabledSite : function(sValue) {
-
+	enabledSite: function (sValue) {
 		var oModel = sap.ui.getCore().getModel("siteModel");
-
 		if (oModel.getProperty("/Rowsets/Rowset/0/Row")) {
-
 			oModel = sap.ui.getCore().getModel("siteModel").oData.Rowsets.Rowset[0].Row;
-
 		} else {
 			oModel = []
-			console.log("no siteModel load");
 		}
-
-		if (oModel.map(function(x) {
-			return x.site_desc;
-		}).indexOf(sValue) != -1) {
-
+		if (oModel.map(function (x) { return x.site_desc; }).indexOf(sValue) != -1) {
 			return true;
-
 		} else {
-
 			return false;
 		}
-
 	}
 
 };
