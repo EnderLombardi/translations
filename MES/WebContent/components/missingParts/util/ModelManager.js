af22ebe9daf8722a8be7d3d60746837c6650a990
Barillon Pierre <pierre.barillon@soprasteria.com>
2017-03-10 14:45:03 +0100

    [missing parts] css

--- MES/WebContent/components/missingParts/util/ModelManager.js
+++ MES/WebContent/components/missingParts/util/ModelManager.js
@@ -39,8 +39,11 @@ airbus.mes.missingParts.util.ModelManager = {
 		if ( oViewModel.oData.Rowsets != undefined ){
 			oViewModel.oData.Rowsets.Rowset[0].Columns.Column.splice(0, 0, { Name: airbus.mes.missingParts.util.Formatter.getTranslation("FilterPlaceholder") });
 		}
-		//var dialog = airbus.mes.missingParts.oView.byId("missingPartsView--missingPartsPopUp");
+		//var dialog =oViewModel.byId("missingPartsView--missingPartsPopUp");
 		//if(dialog) dialog.oPopup.setModal(false);
+		
+		
+		
 		oViewModel.refresh(true);//refresh the model (and so the view)
 	},
 
