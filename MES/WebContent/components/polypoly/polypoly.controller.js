3c3347c1d3a9599a7816877a3df28ddc0a58f583
Nicolas Janvier <nicolas.janvier@soprasteria.com>
2017-03-13 11:47:33 +0100

    [Polypoly]Hide header bar on Popup

--- MES/WebContent/components/polypoly/polypoly.controller.js
+++ MES/WebContent/components/polypoly/polypoly.controller.js
@@ -776,7 +776,7 @@ sap.ui.controller("airbus.mes.polypoly.polypoly",{
 //            var filterUA = new sap.ui.model.Filter("type", "Contains", "UA_");
 //            var aFilters = [filterLabels, filterUA];
 
-            if (nav.getCurrentPage().getId()!="polypolyPage"){
+            if (nav.getCurrentPage().getId()!="polypoly"){
 //                airbus.mes.polypoly.oView.byId("oTablePolypoly").getBinding("rows").filter(aFilters);
                 airbus.mes.polypoly.oView.byId("oTablePolypoly").setFixedRowCount(0);
                 airbus.mes.polypoly.oView.getController().setRowCountVisible(true);
@@ -821,7 +821,7 @@ sap.ui.controller("airbus.mes.polypoly.polypoly",{
         //test
         //console.log("Start :" + sap.ui.getCore().byId("typeRow"))
 
-        var aControlsId = ["AddPolypoly", "navBack"];
+        var aControlsId = ["AddPolypoly", "navBack", "kpiHeaderToolbar"];
         this.setModePoly();
         this.hideContent(aControlsId);
         this.setPolyFilters();
