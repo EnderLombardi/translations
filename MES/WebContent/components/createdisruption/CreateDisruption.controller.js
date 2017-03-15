158598d50a86716409609e733f5ebbd7695e41c1
El Amrani Benoit <benoit.elamrani@soprasteria.com>
2017-03-15 14:30:45 +0100

    [attachment disruption] handle video and picture

--- MES/WebContent/components/createdisruption/CreateDisruption.controller.js
+++ MES/WebContent/components/createdisruption/CreateDisruption.controller.js
@@ -1,7 +1,7 @@
 "use strict";
 //require the base first
 jQuery.sap.require("airbus.mes.disruptions.createDisruptions");
-
+jQuery.sap.require("sap.m.Dialog");
 airbus.mes.disruptions.createDisruptions.extend("airbus.mes.createdisruption.CreateDisruption", {
 
 	/**
@@ -67,7 +67,7 @@ airbus.mes.disruptions.createDisruptions.extend("airbus.mes.createdisruption.Cre
 			this.createDisruptionSettings();
 			sap.ui.getCore().getModel("MaterialListModel").setData([]);
 			sap.ui.getCore().getModel("JigtoolListModel").setData([]);
-			
+
 			sap.ui.getCore().getModel("DesktopFilesModel").setData([]);
 		} else if (sMode == "Edit") {
 			this.loadRsnResponsibleGrp(oData.messageType);
@@ -116,6 +116,23 @@ airbus.mes.disruptions.createDisruptions.extend("airbus.mes.createdisruption.Cre
 
 		this.addFilesToList(filesListBase64.fileName, description, filesListBase64.fileBase64, filesListBase64.size);
 		oInput.destroy();
+	},
+
+	/*
+	 * Local json model is maintained to store the list of attachments 
+	 * attached from the desktop
+	 */
+	onUploadMediaComplete: function (filesListBase64) {
+
+		var oInputComment = sap.ui.getCore().byId("idCommentInput");
+		var oInputName = sap.ui.getCore().byId("idNameInput");
+		var description = oInputComment.getValue();
+		filesListBase64.fileName = oInputName.getValue() + '.' + filesListBase64.type;
+		filesListBase64.size = filesListBase64.size || '';
+
+		this.addFilesToList(filesListBase64.fileName, description, filesListBase64.fileBase64, filesListBase64.size);
+		oInputComment.destroy();
+		oInputName.destroy();
 	},
 
 	addFilesToList: function (fileName, description, fileBase64, size) {
@@ -256,6 +273,218 @@ airbus.mes.disruptions.createDisruptions.extend("airbus.mes.createdisruption.Cre
 			var document = attachedDocumentList[i];
 			airbus.mes.disruptions.ModelManager.attachDocument(ref, document.Title, document.File, document.Description);
 		}
+	},
+	
+	onCameraPress: function (oEvt) {
+		var that = this;
+		var recorder, videoElement;
+		var isRecordingStarted = false;
+		var isStoppedRecording = false;
+
+		that.cameraDialog = new sap.m.Dialog({
+			title: 'Take a video or a picture',
+			content: new sap.m.HBox("video-area"),
+			buttons: [
+				new sap.m.Button({
+					text: 'Record',
+					press: function () {
+						if (that.cameraDialog.getButtons()[0].getText() === 'Record') {
+							launchVideo();
+							that.cameraDialog.getButtons()[0].setText('Stop');
+						} else {
+							stopVideo(function (filesListBase64) {
+								that.cameraDialog.close();
+								that.openCommentDialog(oEvt, filesListBase64);
+							});
+							that.cameraDialog.getButtons()[0].setText('Record');
+						}
+
+					}
+				}),
+				new sap.m.Button({
+					text: 'Take a Picture',
+					press: function () {
+						that.cameraDialog.close();
+						takePicture(function(filesListBase64) {
+							that.openCommentDialog(oEvt, filesListBase64);
+						});
+					}
+				}),
+				new sap.m.Button({
+					text: 'Cancel',
+					press: function () {
+						window.localStream.getVideoTracks()[0].stop();
+						that.cameraDialog.close();
+					}
+				})
+			],
+			afterClose: function () {
+				that.cameraDialog.destroy();
+			}
+		});
+
+		//to get access to the global model
+		this.getView().addDependent(that.cameraDialog);
+
+		that.cameraDialog.open();
+		that.cameraDialog.attachAfterOpen(null, function () {
+			var elementToShare = document.getElementById('video-area');
+			elementToShare.innerHTML = '<video autoplay style="width:100%; height: 100%; position:absolute;right:0px; top:0px; bottom: 0px; background-color:black;"></video>';
+			var canvas2d = document.createElement('canvas');
+			var context = canvas2d.getContext('2d');
+			canvas2d.width = elementToShare.clientWidth;
+			canvas2d.height = elementToShare.clientHeight;
+			canvas2d.style.top = 0;
+			canvas2d.style.left = 0;
+			canvas2d.style.zIndex = -1;
+			(document.body || document.documentElement).appendChild(canvas2d);
+
+			(function looper() {
+				if (!isRecordingStarted) {
+					return setTimeout(looper, 500);
+				}
+				html2canvas(elementToShare, {
+					grabMouse: true,
+					onrendered: function (canvas) {
+						context.clearRect(0, 0, canvas2d.width, canvas2d.height);
+						context.drawImage(canvas, 0, 0, canvas2d.width, canvas2d.height);
+						if (isStoppedRecording) {
+							return;
+						}
+						setTimeout(looper, 1);
+					}
+				});
+			})();
+
+			recorder = RecordRTC(canvas2d, {
+				type: 'canvas',
+				showMousePointer: true
+			});
+
+			function playVideo(callback) {
+				videoElement = document.querySelector('video');
+				function successCallback(stream) {
+					videoElement.onloadedmetadata = function () {
+						callback();
+					};
+					window.localStream = stream;
+					videoElement.src = URL.createObjectURL(stream);
+					videoElement.play();
+				}
+				function errorCallback(error) {
+					console.error('get-user-media error', error);
+					callback();
+				}
+				var mediaConstraints = { video: true, facingMode: { exact: "environment" } };
+				navigator.mediaDevices.getUserMedia(mediaConstraints).then(successCallback).catch(errorCallback);
+			}
+
+			playVideo(function () {
+				console.log('video started');
+			});
+
+		});
+
+		function launchVideo() {
+			isStoppedRecording = false;
+			isRecordingStarted = true;
+			recorder.startRecording();
+		}
+
+		function takePicture(callback) {
+			isStoppedRecording = true;
+			var imageManipulationCanvas = document.createElement('canvas');
+			imageManipulationCanvas.height = 400;
+			imageManipulationCanvas.width = 500;
+			var imageManipulationCtx = imageManipulationCanvas.getContext('2d');
+			imageManipulationCtx.drawImage(videoElement, 0, 0, 500, 400); // Draw image on temporary canvas
+			var dataURL = imageManipulationCanvas.toDataURL();
+			videoElement.pause();
+			window.localStream.getVideoTracks()[0].stop();
+			var filesListBase64 = {};
+			filesListBase64.type = dataURL.substring(dataURL.indexOf('/') + 1, dataURL.indexOf(';'));
+			filesListBase64.fileBase64 = dataURL.substring(dataURL.indexOf('base64,') + 7);
+			imageManipulationCanvas.toBlob(function (blob) {
+				filesListBase64.size = blob.size;
+				callback(filesListBase64);
+			}, 'image/png', 1);
+		}
+
+		function stopVideo(callback) {
+			isStoppedRecording = true;
+			recorder.stopRecording(function (url) {
+				videoElement.pause();
+				window.localStream.getVideoTracks()[0].stop();
+				recorder.getDataURL(function (dataURL) {
+					var filesListBase64 = {};
+					var blob = recorder.getBlob();
+					filesListBase64.type = blob.type.substring(blob.type.indexOf('/') + 1);
+					filesListBase64.fileBase64 = dataURL.substring(dataURL.indexOf('base64,') + 7);
+					filesListBase64.size = blob.size;
+					callback(filesListBase64);
+				});
+			});
+
+		}
+
+
+	},
+
+	openCommentDialog: function (oEvt, filesListBase64) {
+		var dialog = new sap.m.Dialog({
+			customHeader: [new sap.m.Toolbar({
+				content: [new sap.m.Title({
+					text: "{i18nModel>EntDesc}"
+				}).addStyleClass("sapUiSmallMarginBegin")]
+			})],
+			content: [new sap.m.Input("idNameInput", {
+				placeholder: '{i18nModel>fileName}',
+				liveChange: function (oEvt) {
+					var oInputComment = sap.ui.getCore().byId("idCommentInput");
+					var oInputName = sap.ui.getCore().byId("idNameInput");
+					if (oInputComment.getValue().length > 0 && oInputName.getValue().length > 0) {
+						oEvt.getSource().oParent.mAggregations.beginButton.setEnabled(true);
+					} else {
+						oEvt.getSource().oParent.mAggregations.beginButton.setEnabled(false)
+					}
+				}
+			}), new sap.m.Input("idCommentInput", {
+				placeholder: '{i18nModel>description}',
+				liveChange: function (oEvt) {
+					var oInputComment = sap.ui.getCore().byId("idCommentInput");
+					var oInputName = sap.ui.getCore().byId("idNameInput");
+					if (oInputComment.getValue().length > 0 && oInputName.getValue().length > 0) {
+						oEvt.getSource().oParent.mAggregations.beginButton.setEnabled(true);
+					} else {
+						oEvt.getSource().oParent.mAggregations.beginButton.setEnabled(false)
+					}
+				}
+			}),
+			],
+			beginButton: new sap.m.Button({
+				enabled: false,
+				text: '{i18nModel>OK}',
+				press: function () {
+					dialog.close();
+					airbus.mes.createdisruption.oView.oController.onUploadMediaComplete(filesListBase64)
+				}
+			}),
+			endButton: new sap.m.Button({
+				text: '{i18nModel>cancel}',
+				press: function () {
+					dialog.close();
+					var oInputName = sap.ui.getCore().byId("idNameInput");
+					var oInputCommment = sap.ui.getCore().byId("idCommentInput");
+					oInputName.destroy();
+					oInputCommment.destroy();
+				}
+			})
+		});
+
+
+		this.getView().addDependent(dialog);
+		dialog.open();
 	}
+
 
 });
