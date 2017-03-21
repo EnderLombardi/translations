817a7a2a4b4e31d8e793220a76bd127fb58a81f1
Sanjo Thomas <sanjo.thomas@soprasteria.com>
2017-03-21 18:28:51 +0530

    [Logout Defect fix]

--- MES/WebContent/shell/globalNavigation.controller.js
+++ MES/WebContent/shell/globalNavigation.controller.js
@@ -1010,14 +1010,24 @@ sap.ui.controller(
             this.logoutDialog.open();
 
         },
-        onPressLoginUser: function(){         
+        onPressLoginUser: function(){
             
         	var sUrl = airbus.mes.shell.ModelManager.urlModel.getProperty("urllogout");
             sUrl = airbus.mes.shell.ModelManager.replaceURI(sUrl, "$Host", window.location.host);
         	sUrl = airbus.mes.shell.ModelManager.replaceURI(sUrl, "$TimeStamp", Date.now());
                 
         	location.href = sUrl; 
-                  
+//          jQuery.ajax({
+////              url: (airbus.mes.shell.ModelManager.urlModel.getProperty("urllogout") + "&target="+window.location.origin+"/XMII/CM/XX_MOD1684_MES/ui/mes/index.html?saml2=disabled"),
+//        	  url: (airbus.mes.shell.ModelManager.urlModel.getProperty("urllogout")),
+//              type: 'POST',
+//              async: true,
+//              complete: function () {
+////                  location.href = window.location.origin + "/XMII/CM/XX_MOD1684_MES/ui/mes/index.html?saml2=disabled";
+//            	  location.href = window.location.origin + "/XMII/Illuminator?service=Logout&target=" + window.location.origin +"/XMII/CM/XX_MOD1684_MES/ui/mes/index.html?saml2=disabled";
+//              }
+//
+//          })
         },
         
         onPressAutoLogin: function(){
@@ -1026,7 +1036,18 @@ sap.ui.controller(
             sUrl = airbus.mes.shell.ModelManager.replaceURI(sUrl, "$Host", window.location.host);
         	sUrl = airbus.mes.shell.ModelManager.replaceURI(sUrl, "$TimeStamp", Date.now());
                 
-        	location.href = sUrl; 
+        	location.href = sUrl;
+//            jQuery.ajax({
+////                url: (airbus.mes.shell.ModelManager.urlModel.getProperty("urllogout") + "&target="+window.location.origin+"/XMII/CM/XX_MOD1684_MES/ui/mes/index.html?saml2=enabled"),
+//            	url: (airbus.mes.shell.ModelManager.urlModel.getProperty("urllogout")),
+//            	type: 'POST',
+//                async: true,
+//                complete: function () {
+////                    location.href = window.location.origin + "/XMII/CM/XX_MOD1684_MES/ui/mes/index.html?saml2=enabled";
+//                	location.href = window.location.origin + "/XMII/Illuminator?service=Logout&target=" + window.location.origin +"/XMII/CM/XX_MOD1684_MES/ui/mes/index.html?saml2=enabled";
+//                }
+//
+//            })	
         },
         
         onPressCancel: function(){
