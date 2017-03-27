f5958e7d78e71997eb257c4a3bb0a42a4ea11466
Pierandrea Pascolini <pierandrea.pascolini@soprasteria.com>
2017-03-27 10:45:31 +0200

    [Outstanding work]  Airbus QC 1000 Import Outstanding Work Pop-Up: Station filter

--- MES/WebContent/components/stationHandover/util/Formatter.js
+++ MES/WebContent/components/stationHandover/util/Formatter.js
@@ -4,46 +4,41 @@ jQuery.sap.declare("airbus.mes.stationHandover.util.Formatter");
 
 airbus.mes.stationHandover.util.Formatter = {
 
-	translate : function(oEvt) {
-
+	translate: function (oEvt) {
 		return airbus.mes.stationHandover.oView.getModel("stationHandoverI18n").getProperty(oEvt);
-
 	},
 
-	isInsert : function(oEvt) {
-
+	isInsert: function (oEvt) {
 		if (oEvt != null) {
-
 			return String(oEvt.selected) == "true";
-
 		}
 	},
 
-	isInsertEnabled : function(oEvt) {
-
+	isInsertEnabled: function (oEvt) {
 		if (oEvt === "true") {
-
 			return false;
-
 		}
-
 		if (oEvt === "false") {
-
 			return true;
-
 		}
-
 	},
-	
-	displayColor : function(oEvt) {
-		
+
+	displayColor: function (oEvt) {
 		if (oEvt) {
-			
 			return oEvt;
 		} else {
-			
 			return "";
 		}
-		
-	}
+	},
+
+	dateToStringFormat: function (sDate) {
+		// Date send by MII are UTC date
+		var oDate = new Date(sDate);
+		var oFormat = sap.ui.core.format.DateFormat.getInstance({
+			UTC: true,
+			pattern: "dd MMM - HH:mm",
+			calendarType: sap.ui.core.CalendarType.Gregorian
+		});
+		return oFormat.format(oDate)
+	},
 };
